b9677d9372579f53de5c1d8a962f0a92f8eff49f
Jeremy Price <donomii@gmail.com>
2022-09-06 16:46:14 +0200

    Formatting

--- src/test/java/technology/semi/weaviate/integration/client/graphql/ClientGraphQLTest.java
+++ src/test/java/technology/semi/weaviate/integration/client/graphql/ClientGraphQLTest.java
@@ -83,6 +83,36 @@ public class ClientGraphQLTest {
     assertEquals(4, getPizza.size());
   }
 
+
+  @Test
+  public void testRawGQL() {
+    // given
+    Config config = new Config("http", address);
+    WeaviateClient client = new WeaviateClient(config);
+    WeaviateTestGenerics testGenerics = new WeaviateTestGenerics();
+    Field name = Field.builder().name("name").build();
+    // when
+    testGenerics.createTestSchemaAndData(client);
+    Result<GraphQLResponse> result = client.graphQL().rawGQL().withQuery("{Get{Pizza{_additional{id}}}}").run();
+    testGenerics.cleanupWeaviate(client);
+    // then
+    assertNotNull(result);
+    assertFalse(result.hasErrors());
+    GraphQLResponse resp = result.getResult();
+    assertNotNull(resp);
+    assertNotNull(resp.getData());
+    assertTrue(resp.getData() instanceof Map);
+    Map data = (Map) resp.getData();
+    assertNotNull(data.get("Get"));
+    assertTrue(data.get("Get") instanceof Map);
+    Map get = (Map) data.get("Get");
+    assertNotNull(get.get("Pizza"));
+    assertTrue(get.get("Pizza") instanceof List);
+    List getPizza = (List) get.get("Pizza");
+    assertEquals(4, getPizza.size());
+}
+            
+              
   @Test
   public void testGraphQLGetWithNearObjectAndCertainty() {
     // given
@@ -621,36 +651,6 @@ public class ClientGraphQLTest {
     GraphQLResponse resp = result.getResult();
     checkAggregateMetaCount(resp, 1, 4.0d);
   }
-
-  @Test
-  public void testRawGQL() {
-                // given
-                Config config = new Config("http", address);
-                WeaviateClient client = new WeaviateClient(config);
-                WeaviateTestGenerics testGenerics = new WeaviateTestGenerics();
-                Field name = Field.builder().name("name").build();
-                // when
-                testGenerics.createTestSchemaAndData(client);
-                Result<GraphQLResponse> result = client.graphQL().rawGQL().withQuery("{Get{Pizza{_additional{id}}}}").run();
-                testGenerics.cleanupWeaviate(client);
-                // then
-                assertNotNull(result);
-                assertFalse(result.hasErrors());
-                GraphQLResponse resp = result.getResult();
-                assertNotNull(resp);
-                assertNotNull(resp.getData());
-                assertTrue(resp.getData() instanceof Map);
-                Map data = (Map) resp.getData();
-                assertNotNull(data.get("Get"));
-                assertTrue(data.get("Get") instanceof Map);
-                Map get = (Map) data.get("Get");
-                assertNotNull(get.get("Pizza"));
-                assertTrue(get.get("Pizza") instanceof List);
-                List getPizza = (List) get.get("Pizza");
-                assertEquals(4, getPizza.size());
-              }
-            
-
 
   @Test
   public void testGraphQLAggregateWithWhereFilter() {
