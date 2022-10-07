f2fa045cf5263c556ab6ce0df57c04d22a255a8b
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-10-07 14:32:19 +0100

    WVT-361: adds support for /nodes API

--- src/main/java/technology/semi/weaviate/client/v1/misc/Misc.java
+++ src/main/java/technology/semi/weaviate/client/v1/misc/Misc.java
@@ -4,6 +4,7 @@ import technology.semi.weaviate.client.Config;
 import technology.semi.weaviate.client.base.util.DbVersionProvider;
 import technology.semi.weaviate.client.v1.misc.api.LiveChecker;
 import technology.semi.weaviate.client.v1.misc.api.MetaGetter;
+import technology.semi.weaviate.client.v1.misc.api.NodesStatusGetter;
 import technology.semi.weaviate.client.v1.misc.api.OpenIDConfigGetter;
 import technology.semi.weaviate.client.v1.misc.api.ReadyChecker;
 
@@ -30,5 +31,9 @@ public class Misc {
 
   public ReadyChecker readyChecker() {
     return new ReadyChecker(config, dbVersionProvider);
+  }
+
+  public NodesStatusGetter nodesStatusGetter() {
+    return new NodesStatusGetter(config);
   }
 }
