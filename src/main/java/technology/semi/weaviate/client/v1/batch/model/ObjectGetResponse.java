af7b07b0088b353900a086538274611f25385c2b
Sam Stoelinga <sammiestoel@gmail.com>
2022-11-19 10:34:01 -0800

    Change back to Builder instead of SuperBuilder

--- src/main/java/technology/semi/weaviate/client/v1/batch/model/ObjectGetResponse.java
+++ src/main/java/technology/semi/weaviate/client/v1/batch/model/ObjectGetResponse.java
@@ -1,17 +1,17 @@
 package technology.semi.weaviate.client.v1.batch.model;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
-import lombok.experimental.SuperBuilder;
 import technology.semi.weaviate.client.v1.data.model.Deprecation;
 import technology.semi.weaviate.client.v1.data.model.WeaviateObject;
 
 @Getter
 @Setter
-@SuperBuilder
+@Builder
 @ToString(callSuper = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ObjectGetResponse extends WeaviateObject {
