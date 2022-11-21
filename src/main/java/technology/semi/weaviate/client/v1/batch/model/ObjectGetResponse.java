f5928bb01eedf733344dc6391ade368c61f17948
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-11-21 13:15:01 +0100

    replaces @SuperBuilder with constructor @Builder

--- src/main/java/technology/semi/weaviate/client/v1/batch/model/ObjectGetResponse.java
+++ src/main/java/technology/semi/weaviate/client/v1/batch/model/ObjectGetResponse.java
@@ -1,20 +1,28 @@
 package technology.semi.weaviate.client.v1.batch.model;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
-import lombok.experimental.SuperBuilder;
 import technology.semi.weaviate.client.v1.data.model.Deprecation;
 import technology.semi.weaviate.client.v1.data.model.WeaviateObject;
 
+import java.util.Map;
+
 @Getter
-@Setter
-@SuperBuilder
 @ToString(callSuper = true)
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ObjectGetResponse extends WeaviateObject {
   Deprecation[] deprecations;
   ObjectsGetResponseAO2Result result;
+
+  @Builder(builderMethodName = "objectGetResponseBuilder")
+  public ObjectGetResponse(String id, String className, Long creationTimeUnix, Long lastUpdateTimeUnix,
+                           Map<String, Object> properties, Map<String, Object> additional, Float[] vector,
+                           Object vectorWeights, Deprecation[] deprecations, ObjectsGetResponseAO2Result result) {
+    super(id, className, creationTimeUnix, lastUpdateTimeUnix, properties, additional, vector, vectorWeights);
+    this.deprecations = deprecations;
+    this.result = result;
+  }
 }
--- src/main/java/technology/semi/weaviate/client/v1/data/model/WeaviateObject.java
+++ src/main/java/technology/semi/weaviate/client/v1/data/model/WeaviateObject.java
@@ -2,17 +2,19 @@ package technology.semi.weaviate.client.v1.data.model;
 
 import com.google.gson.annotations.SerializedName;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.FieldDefaults;
-import lombok.experimental.SuperBuilder;
 
 import java.util.Map;
 
 @Getter
-@SuperBuilder
+@Builder
 @ToString
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class WeaviateObject {
   String id;
   @SerializedName("class")
