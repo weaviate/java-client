3a1db6b9c00c286999b9e9968c0d2ea588bf8a17
Marcin Antas <antas.marcin@gmail.com>
2022-12-19 12:45:45 +0100

    Fixed client misc test

--- src/test/java/technology/semi/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/technology/semi/weaviate/integration/client/WeaviateVersion.java
@@ -3,9 +3,9 @@ package technology.semi.weaviate.integration.client;
 public class WeaviateVersion {
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "master";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.17.0-prealpha";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "e23d5fd";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "e8b5ccb";
 
   private WeaviateVersion() {}
 }
