7700f7439610fa54721c97b97951b4f7facc8b05
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-12-19 13:11:40 +0100

    adds support for replication factor

--- src/test/java/technology/semi/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/technology/semi/weaviate/integration/client/WeaviateVersion.java
@@ -5,7 +5,7 @@ public class WeaviateVersion {
   // to be set according to weaviate docker image
   public static final String EXPECTED_WEAVIATE_VERSION = "1.17.0-prealpha";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "e8b5ccb";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "29e987d";
 
   private WeaviateVersion() {}
 }
