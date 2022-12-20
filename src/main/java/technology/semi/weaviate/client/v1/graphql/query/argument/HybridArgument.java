c7c67b8adaddc2d4ec9586846ea1e142d44ca099
Marcin Antas <antas.marcin@gmail.com>
2022-12-20 13:18:18 +0100

    Fixed formatting of BM25 and Hybrid arguments

--- src/main/java/technology/semi/weaviate/client/v1/graphql/query/argument/HybridArgument.java
+++ src/main/java/technology/semi/weaviate/client/v1/graphql/query/argument/HybridArgument.java
@@ -8,6 +8,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 import org.apache.commons.lang3.StringUtils;
+
 @Getter
 @Builder
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -19,13 +20,10 @@ public class HybridArgument implements Argument {
   @Override
   public String build() {
     Set<String> arg = new LinkedHashSet<>();
-
     arg.add(String.format("query: \"%s\"", query));
-
     if (vector != null) {
       arg.add(String.format("vector: %s", Arrays.toString(vector)));
     }
-
     if (alpha != null) {
       arg.add(String.format("alpha: %s", alpha));
     }
