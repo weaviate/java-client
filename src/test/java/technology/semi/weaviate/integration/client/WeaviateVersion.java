ecb0fcb789437ebd19707e7fe284da099dcc594b
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-12-21 09:22:14 +0100

    bumps up weaviate to 1.17.0

--- src/test/java/technology/semi/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/technology/semi/weaviate/integration/client/WeaviateVersion.java
@@ -3,9 +3,9 @@ package technology.semi.weaviate.integration.client;
 public class WeaviateVersion {
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.17.0-prealpha";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.17.0";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "8950d6f";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "37d3b17";
 
   private WeaviateVersion() {}
 }
