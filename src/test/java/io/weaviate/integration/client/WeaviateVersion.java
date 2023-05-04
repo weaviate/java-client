4afdf224f30c6e083a81935efe4c31a62dd73942
Andrzej Liszka <andrzej.liszka@gmail.com>
2023-05-04 21:33:53 +0200

    bumps up weaviate version to 1.19

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -3,9 +3,9 @@ package io.weaviate.integration.client;
 public class WeaviateVersion {
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.19.0-beta.1";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.19.0";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "77fe35f";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "48456a1";
 
   private WeaviateVersion() {}
 }
