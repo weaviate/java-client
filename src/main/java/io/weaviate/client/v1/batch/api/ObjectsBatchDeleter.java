a6b6a72c37be02bf4119ba077f77712aedff6307
Andrzej Liszka <andrzej.liszka@gmail.com>
2023-06-15 12:07:09 +0200

    CR improvements

--- src/main/java/io/weaviate/client/v1/batch/api/ObjectsBatchDeleter.java
+++ src/main/java/io/weaviate/client/v1/batch/api/ObjectsBatchDeleter.java
@@ -15,25 +15,25 @@ import io.weaviate.client.v1.filters.WhereFilter;
 
 public class ObjectsBatchDeleter extends BaseClient<BatchDeleteResponse> implements ClientResult<BatchDeleteResponse> {
 
-    private final ObjectsPath objectsPath;
-    private String className;
-    private String consistencyLevel;
-    private String tenantKey;
-    private WhereFilter where;
-    private String output;
-    private Boolean dryRun;
+  private final ObjectsPath objectsPath;
+  private String className;
+  private String consistencyLevel;
+  private String tenantKey;
+  private WhereFilter where;
+  private String output;
+  private Boolean dryRun;
 
 
-    public ObjectsBatchDeleter(HttpClient httpClient, Config config, ObjectsPath objectsPath) {
-        super(httpClient, config);
-        this.objectsPath = objectsPath;
-    }
+  public ObjectsBatchDeleter(HttpClient httpClient, Config config, ObjectsPath objectsPath) {
+    super(httpClient, config);
+    this.objectsPath = objectsPath;
+  }
 
 
-    public ObjectsBatchDeleter withClassName(String className) {
-        this.className = className;
-        return this;
-    }
+  public ObjectsBatchDeleter withClassName(String className) {
+    this.className = className;
+    return this;
+  }
 
   public ObjectsBatchDeleter withConsistencyLevel(String consistencyLevel) {
     this.consistencyLevel = consistencyLevel;
@@ -45,58 +45,58 @@ public class ObjectsBatchDeleter extends BaseClient<BatchDeleteResponse> impleme
     return this;
   }
 
-    public ObjectsBatchDeleter withWhere(WhereFilter where) {
-        this.where = where;
-        return this;
-    }
+  public ObjectsBatchDeleter withWhere(WhereFilter where) {
+    this.where = where;
+    return this;
+  }
 
-    public ObjectsBatchDeleter withOutput(String output) {
-        this.output = output;
-        return this;
-    }
+  public ObjectsBatchDeleter withOutput(String output) {
+    this.output = output;
+    return this;
+  }
 
-    public ObjectsBatchDeleter withDryRun(Boolean dryRun) {
-        this.dryRun = dryRun;
-        return this;
-    }
-
-
-    @Override
-    public Result<BatchDeleteResponse> run() {
-        BatchDeleteMatch match = BatchDeleteMatch.builder()
-                .className(className)
-                .whereFilter(where)
-                .build();
-        BatchDelete batchDelete = BatchDelete.builder()
-                .dryRun(dryRun)
-                .output(output)
-                .match(match)
-                .build();
-        String path = objectsPath.buildDelete(ObjectsPath.Params.builder()
-            .consistencyLevel(consistencyLevel)
-            .tenantKey(tenantKey)
-            .build());
-        Response<BatchDeleteResponse> resp = sendDeleteRequest(path, batchDelete, BatchDeleteResponse.class);
-        return new Result<>(resp);
-    }
+  public ObjectsBatchDeleter withDryRun(Boolean dryRun) {
+    this.dryRun = dryRun;
+    return this;
+  }
 
 
-    @Getter
-    @Builder
-    private static class BatchDelete {
+  @Override
+  public Result<BatchDeleteResponse> run() {
+    BatchDeleteMatch match = BatchDeleteMatch.builder()
+      .className(className)
+      .whereFilter(where)
+      .build();
+    BatchDelete batchDelete = BatchDelete.builder()
+      .dryRun(dryRun)
+      .output(output)
+      .match(match)
+      .build();
+    String path = objectsPath.buildDelete(ObjectsPath.Params.builder()
+      .consistencyLevel(consistencyLevel)
+      .tenantKey(tenantKey)
+      .build());
+    Response<BatchDeleteResponse> resp = sendDeleteRequest(path, batchDelete, BatchDeleteResponse.class);
+    return new Result<>(resp);
+  }
 
-        BatchDeleteMatch match;
-        String output;
-        Boolean dryRun;
-    }
 
-    @Getter
-    @Builder
-    private static class BatchDeleteMatch {
+  @Getter
+  @Builder
+  private static class BatchDelete {
 
-        @SerializedName("class")
-        String className;
-        @SerializedName("where")
-        WhereFilter whereFilter;
-    }
+    BatchDeleteMatch match;
+    String output;
+    Boolean dryRun;
+  }
+
+  @Getter
+  @Builder
+  private static class BatchDeleteMatch {
+
+    @SerializedName("class")
+    String className;
+    @SerializedName("where")
+    WhereFilter whereFilter;
+  }
 }
