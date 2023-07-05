f66dec9700a0506aa2b3758d6b44efe86b2b58cc
Andrzej Liszka <andrzej.liszka@gmail.com>
2023-07-05 17:02:35 +0200

    updates weaviate version

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -3,9 +3,9 @@ package io.weaviate.integration.client;
 public class WeaviateVersion {
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.20.0-prealpha";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.20.0";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "2216131";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "7865a4a";
 
   private WeaviateVersion() {}
 }
