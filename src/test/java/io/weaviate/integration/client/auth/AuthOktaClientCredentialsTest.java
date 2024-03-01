8558d2b54a784ca1edc412961cac526f9b382f82
Eddú Meléndez <eddu.melendez@gmail.com>
2024-03-01 16:23:15 -0600

    Move more tests

--- src/test/java/io/weaviate/integration/client/auth/AuthOktaClientCredentialsTest.java
+++ src/test/java/io/weaviate/integration/client/auth/AuthOktaClientCredentialsTest.java
@@ -1,7 +1,8 @@
 package io.weaviate.integration.client.auth;
 
-import java.io.File;
 import java.util.Arrays;
+
+import io.weaviate.integration.client.WeaviateWithOktaCcContainer;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -9,29 +10,25 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import io.weaviate.client.Config;
 import io.weaviate.client.WeaviateClient;
 import io.weaviate.client.base.Result;
 import io.weaviate.client.v1.auth.ClientCredentialsFlow;
 import io.weaviate.client.v1.auth.exception.AuthException;
 import io.weaviate.client.v1.misc.model.Meta;
+import org.testcontainers.weaviate.WeaviateContainer;
+
 import static io.weaviate.integration.client.WeaviateVersion.EXPECTED_WEAVIATE_VERSION;
 
 public class AuthOktaClientCredentialsTest {
   private String address;
 
   @ClassRule
-  public static DockerComposeContainer compose = new DockerComposeContainer(
-    new File("src/test/resources/docker-compose-okta-cc.yaml")
-  ).withExposedService("weaviate-auth-okta-cc_1", 8082, Wait.forHttp("/v1/.well-known/openid-configuration").forStatusCode(200));
+  public static WeaviateContainer weaviate = new WeaviateWithOktaCcContainer("semitechnologies/weaviate:1.23.1");
 
   @Before
   public void before() {
-    String host = compose.getServiceHost("weaviate-auth-okta-cc_1", 8082);
-    Integer port = compose.getServicePort("weaviate-auth-okta-cc_1", 8082);
-    address = host + ":" + port;
+    address = weaviate.getHttpHostAddress();
   }
 
   @Test
