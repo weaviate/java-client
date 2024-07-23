7743091f645c594780e73e02e0927424a4e44c40
Marcin Antas <antas.marcin@gmail.com>
2024-07-23 09:56:34 +0200

    Swithch to Weaviate v1.26.0 in tests

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -3,12 +3,12 @@ package io.weaviate.integration.client;
 public class WeaviateVersion {
 
   // docker image version
-  public static final String WEAVIATE_IMAGE = "1.26.0-rc.1";
+  public static final String WEAVIATE_IMAGE = "1.26.0";
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.26.0-rc.1";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.26.0";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "1f9b2bf";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "619e6ea";
 
   private WeaviateVersion() {}
 }
