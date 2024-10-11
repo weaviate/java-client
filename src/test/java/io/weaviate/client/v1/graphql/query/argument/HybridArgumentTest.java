0dd43186541cf30d9cf19e29dcd4058f9c7610e1
dyma solovei <dmytro.y.solovei@gmail.com>
2024-10-11 08:04:04 +0200

    test: rewrite to JUnit4 format, remove added dependency

--- src/test/java/io/weaviate/client/v1/graphql/query/argument/HybridArgumentTest.java
+++ src/test/java/io/weaviate/client/v1/graphql/query/argument/HybridArgumentTest.java
@@ -3,79 +3,88 @@ package io.weaviate.client.v1.graphql.query.argument;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.LinkedHashMap;
-import java.util.stream.Stream;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.jparams.junit4.JParamsTestRunner;
+import com.jparams.junit4.data.DataMethod;
+
+@RunWith(JParamsTestRunner.class)
 public class HybridArgumentTest {
-        static Stream<Arguments> testCases() {
-                return Stream.of(
-                                Arguments.of(
+        public static Object[][] provideTestCases() {
+                return new Object[][] {
+                                {
                                                 "simple query",
                                                 HybridArgument.builder()
                                                                 .query("I'm a simple string")
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\"}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\"}"
+                                },
+                                {
                                                 "vector and alpha",
                                                 HybridArgument.builder()
                                                                 .query("I'm a simple string")
                                                                 .vector(new Float[] { .1f, .2f, .3f })
                                                                 .alpha(.567f)
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" vector:[0.1,0.2,0.3] alpha:0.567}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\" vector:[0.1,0.2,0.3] alpha:0.567}"
+                                },
+                                {
                                                 "vector and target vectors",
                                                 HybridArgument.builder()
                                                                 .query("I'm a simple string")
                                                                 .vector(new Float[] { .1f, .2f, .3f })
                                                                 .targetVectors(new String[] { "vector1" })
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" vector:[0.1,0.2,0.3] targetVectors:[\"vector1\"]}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\" vector:[0.1,0.2,0.3] targetVectors:[\"vector1\"]}"
+                                },
+                                {
                                                 "with escaped characters",
                                                 HybridArgument.builder()
                                                                 .query("\"I'm a complex string\" says the {'`:string:`'}")
                                                                 .build(),
-                                                "hybrid:{query:\"\\\"I'm a complex string\\\" says the {'`:string:`'}\"}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"\\\"I'm a complex string\\\" says the {'`:string:`'}\"}"
+                                },
+                                {
                                                 "fusion type ranked",
                                                 HybridArgument.builder()
                                                                 .query("I'm a simple string")
                                                                 .fusionType(FusionType.RANKED)
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" fusionType:rankedFusion}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\" fusionType:rankedFusion}"
+                                },
+                                {
                                                 "fusion type relative score",
                                                 HybridArgument.builder()
                                                                 .query("I'm a simple string")
                                                                 .fusionType(FusionType.RELATIVE_SCORE)
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" fusionType:relativeScoreFusion}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\" fusionType:relativeScoreFusion}"
+                                },
+                                {
                                                 "specify properties to search on",
                                                 HybridArgument.builder()
                                                                 .query("I'm a simple string")
                                                                 .properties(new String[] { "prop1", "prop2" })
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" properties:[\"prop1\",\"prop2\"]}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\" properties:[\"prop1\",\"prop2\"]}"
+                                },
+                                {
                                                 "nearVector search",
                                                 HybridArgument.builder().query("I'm a simple string")
-                                                                .searches(HybridArgument.Searches.builder()
-                                                                                .nearVector(NearVectorArgument
-                                                                                                .builder()
-                                                                                                // @formatter:off
-                                                                                                .vector(new Float[] {.1f, .2f, .3f })
-                                                                                                // @formatter:on
-                                                                                                .certainty(0.9f)
-                                                                                                .build())
-                                                                                .build())
+                                                // @formatter:off
+                                                        .searches(HybridArgument.Searches.builder()
+                                                                .nearVector(NearVectorArgument.builder()
+                                                                        .vector(new Float[] {.1f, .2f, .3f })
+                                                                        .certainty(0.9f)
+                                                                        .build())
+                                                                .build())
+                                                // @formatter:on
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" searches:{nearVector:{vector:[0.1,0.2,0.3] certainty:0.9}}}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\" searches:{nearVector:{vector:[0.1,0.2,0.3] certainty:0.9}}}"
+                                },
+                                {
                                                 "nearText search",
                                                 HybridArgument.builder().query("I'm a simple string")
                                                 // @formatter:off
@@ -88,8 +97,9 @@ public class HybridArgumentTest {
                                                                         .build())
                                                 // @formatter:on
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" searches:{nearText:{concepts:[\"concept\"] certainty:0.9}}}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\" searches:{nearText:{concepts:[\"concept\"] certainty:0.9}}}"
+                                },
+                                {
                                                 "target vectors",
                                                 HybridArgument.builder().query("I'm a simple string")
                                                                 .targets(Targets.builder()
@@ -103,8 +113,9 @@ public class HybridArgumentTest {
                                                                         .build())
                                                                 // @formatter:on
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" targets:{combinationMethod:minimum targetVectors:[\"t1\",\"t2\"] weights:{t1:0.8 t2:0.2}}}"),
-                                Arguments.of(
+                                                "hybrid:{query:\"I'm a simple string\" targets:{combinationMethod:minimum targetVectors:[\"t1\",\"t2\"] weights:{t1:0.8 t2:0.2}}}"
+                                },
+                                {
                                                 "max vector distance",
                                                 HybridArgument.builder().query("I'm a simple string")
                                                 // @formatter:off
@@ -114,12 +125,14 @@ public class HybridArgumentTest {
                                                 .maxVectorDistance(.5f)
                                                 // @formatter:on
                                                                 .build(),
-                                                "hybrid:{query:\"I'm a simple string\" maxVectorDistance:0.5 searches:{nearText:{concepts:[\"concept\"]}}}"));
+                                                "hybrid:{query:\"I'm a simple string\" maxVectorDistance:0.5 searches:{nearText:{concepts:[\"concept\"]}}}"
+                                }
+                };
         }
 
-        @ParameterizedTest
-        @MethodSource("testCases")
-        void test(String name, HybridArgument hybrid, String expected) {
+        @DataMethod(source = HybridArgumentTest.class, method = "provideData")
+        @Test
+        public void test(String name, HybridArgument hybrid, String expected) throws Exception {
                 String actual = hybrid.build();
                 assertThat(actual).as(name).isEqualTo(expected);
         }
