fe4ca6dace6e8feb1706ae331d204120eef69482
dyma solovei <dmytro.y.solovei@gmail.com>
2024-11-15 10:05:28 +0100

    chore: fix formatting

--- src/main/java/io/weaviate/client/v1/async/graphql/api/Explore.java
+++ src/main/java/io/weaviate/client/v1/async/graphql/api/Explore.java
@@ -1,10 +1,5 @@
 package io.weaviate.client.v1.async.graphql.api;
 
-import java.util.concurrent.Future;
-
-import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
-import org.apache.hc.core5.concurrent.FutureCallback;
-
 import io.weaviate.client.Config;
 import io.weaviate.client.base.AsyncBaseClient;
 import io.weaviate.client.base.AsyncClientResult;
@@ -12,95 +7,93 @@ import io.weaviate.client.base.Result;
 import io.weaviate.client.v1.graphql.model.ExploreFields;
 import io.weaviate.client.v1.graphql.model.GraphQLQuery;
 import io.weaviate.client.v1.graphql.model.GraphQLResponse;
-import io.weaviate.client.v1.graphql.query.argument.AskArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearAudioArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearDepthArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearImageArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearImuArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearObjectArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearTextArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearThermalArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearVectorArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearVideoArgument;
+import io.weaviate.client.v1.graphql.query.argument.*;
 import io.weaviate.client.v1.graphql.query.builder.ExploreBuilder;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.core5.concurrent.FutureCallback;
+
+import java.util.concurrent.Future;
 
 public class Explore extends AsyncBaseClient<GraphQLResponse> implements AsyncClientResult<GraphQLResponse> {
-    private final ExploreBuilder.ExploreBuilderBuilder exploreBuilder;
+  private final ExploreBuilder.ExploreBuilderBuilder exploreBuilder;
 
-    public Explore(CloseableHttpAsyncClient client, Config config) {
-        super(client, config);
-        exploreBuilder = ExploreBuilder.builder();
-    }
+  public Explore(CloseableHttpAsyncClient client, Config config) {
+    super(client, config);
+    exploreBuilder = ExploreBuilder.builder();
+  }
 
-    public Explore withFields(ExploreFields... fields) {
-        exploreBuilder.fields(fields);
-        return this;
-    }
+  public Explore withFields(ExploreFields... fields) {
+    exploreBuilder.fields(fields);
+    return this;
+  }
 
-    public Explore withLimit(Integer limit) {
-        exploreBuilder.limit(limit);
-        return this;
-    }
+  public Explore withLimit(Integer limit) {
+    exploreBuilder.limit(limit);
+    return this;
+  }
 
-    public Explore withOffset(Integer offset) {
-        exploreBuilder.offset(offset);
-        return this;
-    }
+  public Explore withOffset(Integer offset) {
+    exploreBuilder.offset(offset);
+    return this;
+  }
 
-    public Explore withAsk(AskArgument ask) {
-        exploreBuilder.withAskArgument(ask);
-        return this;
-    }
+  public Explore withAsk(AskArgument ask) {
+    exploreBuilder.withAskArgument(ask);
+    return this;
+  }
 
-    public Explore withNearText(NearTextArgument nearText) {
-        exploreBuilder.withNearText(nearText);
-        return this;
-    }
+  public Explore withNearText(NearTextArgument nearText) {
+    exploreBuilder.withNearText(nearText);
+    return this;
+  }
 
-    public Explore withNearObject(NearObjectArgument nearObject) {
-        exploreBuilder.withNearObjectFilter(nearObject);
-        return this;
-    }
+  public Explore withNearObject(NearObjectArgument nearObject) {
+    exploreBuilder.withNearObjectFilter(nearObject);
+    return this;
+  }
 
-    public Explore withNearVector(NearVectorArgument nearVector) {
-        exploreBuilder.withNearVectorFilter(nearVector);
-        return this;
-    }
+  public Explore withNearVector(NearVectorArgument nearVector) {
+    exploreBuilder.withNearVectorFilter(nearVector);
+    return this;
+  }
 
-    public Explore withNearImage(NearImageArgument nearImage) {
-        exploreBuilder.withNearImageFilter(nearImage);
-        return this;
-    }
+  public Explore withNearImage(NearImageArgument nearImage) {
+    exploreBuilder.withNearImageFilter(nearImage);
+    return this;
+  }
 
-    public Explore withNearAudio(NearAudioArgument nearAudio) {
-        exploreBuilder.withNearAudioFilter(nearAudio);
-        return this;
-    }
+  public Explore withNearAudio(NearAudioArgument nearAudio) {
+    exploreBuilder.withNearAudioFilter(nearAudio);
+    return this;
+  }
 
-    public Explore withNearVideo(NearVideoArgument nearVideo) {
-        exploreBuilder.withNearVideoFilter(nearVideo);
-        return this;
-    }
+  public Explore withNearVideo(NearVideoArgument nearVideo) {
+    exploreBuilder.withNearVideoFilter(nearVideo);
+    return this;
+  }
 
-    public Explore withNearDepth(NearDepthArgument nearDepth) {
-        exploreBuilder.withNearDepthFilter(nearDepth);
-        return this;
-    }
+  public Explore withNearDepth(NearDepthArgument nearDepth) {
+    exploreBuilder.withNearDepthFilter(nearDepth);
+    return this;
+  }
 
-    public Explore withNearThermal(NearThermalArgument nearThermal) {
-        exploreBuilder.withNearThermalFilter(nearThermal);
-        return this;
-    }
+  public Explore withNearThermal(NearThermalArgument nearThermal) {
+    exploreBuilder.withNearThermalFilter(nearThermal);
+    return this;
+  }
 
-    public Explore withNearImu(NearImuArgument nearImu) {
-        exploreBuilder.withNearImuFilter(nearImu);
-        return this;
-    }
+  public Explore withNearImu(NearImuArgument nearImu) {
+    exploreBuilder.withNearImuFilter(nearImu);
+    return this;
+  }
 
-    @Override
-    public Future<Result<GraphQLResponse>> run(FutureCallback<Result<GraphQLResponse>> callback) {
-        String exploreQuery = exploreBuilder.build().buildQuery();
-        GraphQLQuery query = GraphQLQuery.builder().query(exploreQuery).build();
-        return sendPostRequest("/graphql", query, GraphQLResponse.class, callback);
-    }
+  @Override
+  public Future<Result<GraphQLResponse>> run(FutureCallback<Result<GraphQLResponse>> callback) {
+    String exploreQuery = exploreBuilder.build()
+      .buildQuery();
+    GraphQLQuery query = GraphQLQuery.builder()
+      .query(exploreQuery)
+      .build();
+    return sendPostRequest("/graphql", query, GraphQLResponse.class, callback);
+  }
 }
--- src/main/java/io/weaviate/client/v1/async/graphql/api/Get.java
+++ src/main/java/io/weaviate/client/v1/async/graphql/api/Get.java
@@ -1,10 +1,5 @@
 package io.weaviate.client.v1.async.graphql.api;
 
-import java.util.concurrent.Future;
-
-import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
-import org.apache.hc.core5.concurrent.FutureCallback;
-
 import io.weaviate.client.Config;
 import io.weaviate.client.base.AsyncBaseClient;
 import io.weaviate.client.base.AsyncClientResult;
@@ -12,170 +7,167 @@ import io.weaviate.client.base.Result;
 import io.weaviate.client.v1.filters.WhereFilter;
 import io.weaviate.client.v1.graphql.model.GraphQLQuery;
 import io.weaviate.client.v1.graphql.model.GraphQLResponse;
-import io.weaviate.client.v1.graphql.query.argument.AskArgument;
-import io.weaviate.client.v1.graphql.query.argument.Bm25Argument;
-import io.weaviate.client.v1.graphql.query.argument.GroupArgument;
-import io.weaviate.client.v1.graphql.query.argument.GroupByArgument;
-import io.weaviate.client.v1.graphql.query.argument.HybridArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearAudioArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearDepthArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearImageArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearImuArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearObjectArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearTextArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearThermalArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearVectorArgument;
-import io.weaviate.client.v1.graphql.query.argument.NearVideoArgument;
-import io.weaviate.client.v1.graphql.query.argument.SortArgument;
-import io.weaviate.client.v1.graphql.query.argument.SortArguments;
-import io.weaviate.client.v1.graphql.query.argument.WhereArgument;
+import io.weaviate.client.v1.graphql.query.argument.*;
 import io.weaviate.client.v1.graphql.query.builder.GetBuilder;
 import io.weaviate.client.v1.graphql.query.fields.Field;
 import io.weaviate.client.v1.graphql.query.fields.Fields;
 import io.weaviate.client.v1.graphql.query.fields.GenerativeSearchBuilder;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.core5.concurrent.FutureCallback;
+
+import java.util.concurrent.Future;
 
 public class Get extends AsyncBaseClient<GraphQLResponse> implements AsyncClientResult<GraphQLResponse> {
-    private final GetBuilder.GetBuilderBuilder getBuilder;
+  private final GetBuilder.GetBuilderBuilder getBuilder;
 
-    public Get(CloseableHttpAsyncClient client, Config config) {
-        super(client, config);
-        getBuilder = GetBuilder.builder();
-    }
+  public Get(CloseableHttpAsyncClient client, Config config) {
+    super(client, config);
+    getBuilder = GetBuilder.builder();
+  }
 
-    public Get withClassName(String className) {
-        getBuilder.className(className);
-        return this;
-    }
+  public Get withClassName(String className) {
+    getBuilder.className(className);
+    return this;
+  }
 
-    public Get withFields(Field... fields) {
-        getBuilder.fields(Fields.builder().fields(fields).build());
-        return this;
-    }
+  public Get withFields(Field... fields) {
+    getBuilder.fields(Fields.builder()
+      .fields(fields)
+      .build());
+    return this;
+  }
 
-    @Deprecated
-    public Get withWhere(WhereFilter where) {
-        return withWhere(WhereArgument.builder().filter(where).build());
-    }
+  @Deprecated
+  public Get withWhere(WhereFilter where) {
+    return withWhere(WhereArgument.builder()
+      .filter(where)
+      .build());
+  }
 
-    public Get withWhere(WhereArgument where) {
-        getBuilder.withWhereFilter(where);
-        return this;
-    }
+  public Get withWhere(WhereArgument where) {
+    getBuilder.withWhereFilter(where);
+    return this;
+  }
 
-    public Get withLimit(Integer limit) {
-        getBuilder.limit(limit);
-        return this;
-    }
+  public Get withLimit(Integer limit) {
+    getBuilder.limit(limit);
+    return this;
+  }
 
-    public Get withOffset(Integer offset) {
-        getBuilder.offset(offset);
-        return this;
-    }
+  public Get withOffset(Integer offset) {
+    getBuilder.offset(offset);
+    return this;
+  }
 
-    public Get withAfter(String after) {
-        getBuilder.after(after);
-        return this;
-    }
+  public Get withAfter(String after) {
+    getBuilder.after(after);
+    return this;
+  }
 
-    public Get withBm25(Bm25Argument bm25) {
-        getBuilder.withBm25Filter(bm25);
-        return this;
-    }
+  public Get withBm25(Bm25Argument bm25) {
+    getBuilder.withBm25Filter(bm25);
+    return this;
+  }
 
-    public Get withHybrid(HybridArgument hybrid) {
-        getBuilder.withHybridFilter(hybrid);
-        return this;
-    }
+  public Get withHybrid(HybridArgument hybrid) {
+    getBuilder.withHybridFilter(hybrid);
+    return this;
+  }
 
-    public Get withAsk(AskArgument ask) {
-        getBuilder.withAskArgument(ask);
-        return this;
-    }
+  public Get withAsk(AskArgument ask) {
+    getBuilder.withAskArgument(ask);
+    return this;
+  }
 
-    public Get withNearText(NearTextArgument nearText) {
-        getBuilder.withNearTextFilter(nearText);
-        return this;
-    }
+  public Get withNearText(NearTextArgument nearText) {
+    getBuilder.withNearTextFilter(nearText);
+    return this;
+  }
 
-    public Get withNearObject(NearObjectArgument nearObject) {
-        getBuilder.withNearObjectFilter(nearObject);
-        return this;
-    }
+  public Get withNearObject(NearObjectArgument nearObject) {
+    getBuilder.withNearObjectFilter(nearObject);
+    return this;
+  }
 
-    public Get withNearVector(NearVectorArgument nearVector) {
-        getBuilder.withNearVectorFilter(nearVector);
-        return this;
-    }
+  public Get withNearVector(NearVectorArgument nearVector) {
+    getBuilder.withNearVectorFilter(nearVector);
+    return this;
+  }
 
-    public Get withNearImage(NearImageArgument nearImage) {
-        getBuilder.withNearImageFilter(nearImage);
-        return this;
-    }
+  public Get withNearImage(NearImageArgument nearImage) {
+    getBuilder.withNearImageFilter(nearImage);
+    return this;
+  }
 
-    public Get withNearAudio(NearAudioArgument nearAudio) {
-        getBuilder.withNearAudioFilter(nearAudio);
-        return this;
-    }
+  public Get withNearAudio(NearAudioArgument nearAudio) {
+    getBuilder.withNearAudioFilter(nearAudio);
+    return this;
+  }
 
-    public Get withNearVideo(NearVideoArgument nearVideo) {
-        getBuilder.withNearVideoFilter(nearVideo);
-        return this;
-    }
+  public Get withNearVideo(NearVideoArgument nearVideo) {
+    getBuilder.withNearVideoFilter(nearVideo);
+    return this;
+  }
 
-    public Get withNearDepth(NearDepthArgument nearDepth) {
-        getBuilder.withNearDepthFilter(nearDepth);
-        return this;
-    }
+  public Get withNearDepth(NearDepthArgument nearDepth) {
+    getBuilder.withNearDepthFilter(nearDepth);
+    return this;
+  }
 
-    public Get withNearThermal(NearThermalArgument nearThermal) {
-        getBuilder.withNearThermalFilter(nearThermal);
-        return this;
-    }
+  public Get withNearThermal(NearThermalArgument nearThermal) {
+    getBuilder.withNearThermalFilter(nearThermal);
+    return this;
+  }
 
-    public Get withNearImu(NearImuArgument nearImu) {
-        getBuilder.withNearImuFilter(nearImu);
-        return this;
-    }
+  public Get withNearImu(NearImuArgument nearImu) {
+    getBuilder.withNearImuFilter(nearImu);
+    return this;
+  }
 
-    public Get withGroup(GroupArgument group) {
-        getBuilder.withGroupArgument(group);
-        return this;
-    }
+  public Get withGroup(GroupArgument group) {
+    getBuilder.withGroupArgument(group);
+    return this;
+  }
 
-    public Get withSort(SortArgument... sort) {
-        getBuilder.withSortArguments(SortArguments.builder().sort(sort).build());
-        return this;
-    }
+  public Get withSort(SortArgument... sort) {
+    getBuilder.withSortArguments(SortArguments.builder()
+      .sort(sort)
+      .build());
+    return this;
+  }
 
-    public Get withGenerativeSearch(GenerativeSearchBuilder generativeSearch) {
-        getBuilder.withGenerativeSearch(generativeSearch);
-        return this;
-    }
+  public Get withGenerativeSearch(GenerativeSearchBuilder generativeSearch) {
+    getBuilder.withGenerativeSearch(generativeSearch);
+    return this;
+  }
 
-    public Get withConsistencyLevel(String level) {
-        getBuilder.withConsistencyLevel(level);
-        return this;
-    }
+  public Get withConsistencyLevel(String level) {
+    getBuilder.withConsistencyLevel(level);
+    return this;
+  }
 
-    public Get withGroupBy(GroupByArgument groupBy) {
-        getBuilder.withGroupByArgument(groupBy);
-        return this;
-    }
+  public Get withGroupBy(GroupByArgument groupBy) {
+    getBuilder.withGroupByArgument(groupBy);
+    return this;
+  }
 
-    public Get withTenant(String tenant) {
-        getBuilder.tenant(tenant);
-        return this;
-    }
+  public Get withTenant(String tenant) {
+    getBuilder.tenant(tenant);
+    return this;
+  }
 
-    public Get withAutocut(Integer autocut) {
-        getBuilder.autocut(autocut);
-        return this;
-    }
+  public Get withAutocut(Integer autocut) {
+    getBuilder.autocut(autocut);
+    return this;
+  }
 
-    @Override
-    public Future<Result<GraphQLResponse>> run(FutureCallback<Result<GraphQLResponse>> callback) {
-        String getQuery = getBuilder.build().buildQuery();
-        GraphQLQuery query = GraphQLQuery.builder().query(getQuery).build();
-        return sendPostRequest("/graphql", query, GraphQLResponse.class, callback);
-    }
+  @Override
+  public Future<Result<GraphQLResponse>> run(FutureCallback<Result<GraphQLResponse>> callback) {
+    String getQuery = getBuilder.build()
+      .buildQuery();
+    GraphQLQuery query = GraphQLQuery.builder()
+      .query(getQuery)
+      .build();
+    return sendPostRequest("/graphql", query, GraphQLResponse.class, callback);
+  }
 }
