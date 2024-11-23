001c539f0fcfb3fc8d5ff73708d0e25c4c17b7da
Andrzej Liszka <andrzej.liszka@gmail.com>
2024-11-23 23:21:15 +0100

    test 3.1 print

--- src/test/java/io/weaviate/integration/client/async/batch/ClientBatchCreateMockServerTest.java
+++ src/test/java/io/weaviate/integration/client/async/batch/ClientBatchCreateMockServerTest.java
@@ -298,14 +298,14 @@ public class ClientBatchCreateMockServerTest {
           .build(),
         1
       },
-      new Object[]{
-        // final response should be available after 1 retry (200 ms)
-        ObjectsBatcher.BatchRetriesConfig.defaultConfig()
-          .retriesIntervalMs(200)
-          .maxTimeoutRetries(1)
-          .build(),
-        2
-      },
+//      new Object[]{
+//        // final response should be available after 1 retry (200 ms)
+//        ObjectsBatcher.BatchRetriesConfig.defaultConfig()
+//          .retriesIntervalMs(200)
+//          .maxTimeoutRetries(1)
+//          .build(),
+//        2
+//      },
 //      new Object[]{
 //        // final response should be available after 2 retries (200 + 400 ms)
 //        ObjectsBatcher.BatchRetriesConfig.defaultConfig()
