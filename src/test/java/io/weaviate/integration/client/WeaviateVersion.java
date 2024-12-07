a4b43eeaf2468f8e435bd39333d9f088f07b6981
dyma solovei <dmytro.y.solovei@gmail.com>
2024-12-07 17:36:49 +0100

    test: update expected Weaviate version

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -6,7 +6,7 @@ public class WeaviateVersion {
   public static final String WEAVIATE_IMAGE = "stable-v1.28-ac93b01";
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.28.0";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.28.0-rc.0";
   // to be set according to weaviate docker image
   public static final String EXPECTED_WEAVIATE_GIT_HASH = "ac93b01";
 
