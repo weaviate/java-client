ad6cb545ec1fa5c490ddd28ca078bf921390a679
dyma solovei <dmytro.y.solovei@gmail.com>
2024-12-09 17:31:28 +0100

    ci: target Weaviate v1.27.7

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -3,12 +3,12 @@ package io.weaviate.integration.client;
 public class WeaviateVersion {
 
   // docker image version
-  public static final String WEAVIATE_IMAGE = "stable-v1.28-ac93b01";
+  public static final String WEAVIATE_IMAGE = "stable-v1.27.7";
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.28.0-rc.0";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.27.7";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "ac93b01";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "a6d7f16";
 
   private WeaviateVersion() {
   }
