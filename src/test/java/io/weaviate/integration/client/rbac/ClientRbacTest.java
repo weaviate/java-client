33c430637dcbb9e1b8e8eb6dc21c41dbe8bd8b9b
dyma solovei <dmytro.y.solovei@gmail.com>
2025-01-23 17:59:36 +0100

    test: add test for assinging and revoking roles

--- src/test/java/io/weaviate/integration/client/rbac/ClientRbacTest.java
+++ src/test/java/io/weaviate/integration/client/rbac/ClientRbacTest.java
@@ -1,11 +1,11 @@
 package io.weaviate.integration.client.rbac;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.list;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import java.util.List;
@@ -19,7 +19,6 @@ import org.junit.rules.TestName;
 
 import io.weaviate.client.Config;
 import io.weaviate.client.WeaviateAuthClient;
-import io.weaviate.client.WeaviateClient;
 import io.weaviate.client.base.Result;
 import io.weaviate.client.v1.auth.exception.AuthException;
 import io.weaviate.client.v1.rbac.Roles;
@@ -69,7 +68,7 @@ public class ClientRbacTest {
     Result<List<Role>> response = roles.allGetter().run();
     List<Role> all = response.getResult();
 
-    assertThat(response.getError()).as("result had errors").isNull();
+    assertThat(response.getError()).as("get all roles error").isNull();
     assertThat(all).hasSize(2).as("wrong number of roles");
     assertThat(all.get(0)).returns(adminRole, Role::getName);
     assertThat(all.get(1)).returns(viewerRole, Role::getName);
@@ -78,14 +77,23 @@ public class ClientRbacTest {
   @Test
   public void testGetUserRoles() {
     Result<List<Role>> responseCurrent = roles.userRolesGetter().run();
-    assertThat(responseCurrent.getError()).as("result had errors").isNull();
+    assertThat(responseCurrent.getError()).as("get roles for current user error").isNull();
     Result<List<Role>> responseAdminUser = roles.userRolesGetter().withUser(adminUser).run();
-    assertThat(responseAdminUser.getError()).as("result had errors").isNull();
+    assertThat(responseAdminUser.getError()).as("get roles for user error").isNull();
 
     List<Role> currentRoles = responseCurrent.getResult();
     List<Role> adminRoles = responseAdminUser.getResult();
 
     Assertions.assertArrayEquals(currentRoles.toArray(), adminRoles.toArray(), "expect same set of roles");
+  }
+
+  public void testGetAssignedUsers() {
+    Result<List<String>> response = roles.assignedUsersGetter().withRole(adminRole).run();
+    assertThat(response.getError()).as("get assigned users error").isNull();
+
+    List<String> users = response.getResult();
+    assertThat(users).as("users assigned to " + adminRole + " role").hasSize(1);
+    assertEquals(adminUser, users.get(0), "wrong user assinged to " + adminRole + " role");
   }
 
   // TODO: check if I can create a role with a name that's not a valid URL
@@ -127,7 +135,7 @@ public class ClientRbacTest {
       }
     } finally {
       roles.deleter().withName(myRole).run();
-      assertFalse("should not exist after deletion", checkRoleExists(myRole));
+      assertFalse("role should not exist after deletion", checkRoleExists(myRole));
     }
   }
 
@@ -143,16 +151,16 @@ public class ClientRbacTest {
       assumeTrue(checkRoleExists(myRole), "role should exist after creation");
 
       // Act
-      Result<?> addResult = roles.permissionAdder().withRole(myRole)
+      Result<?> response = roles.permissionAdder().withRole(myRole)
           .withPermissions(toAdd)
           .run();
-      assertNull("add-permissions operation error", addResult.getError());
+      assertNull("add-permissions operation error", response.getError());
 
       // Assert
       assertTrue("should have permission " + toAdd, checkHasPermission(myRole, toAdd));
     } finally {
       roles.deleter().withName(myRole).run();
-      assertFalse("should not exist after deletion", checkRoleExists(myRole));
+      assertFalse("role should not exist after deletion", checkRoleExists(myRole));
     }
   }
 
@@ -170,16 +178,64 @@ public class ClientRbacTest {
       assumeTrue(checkRoleExists(myRole), "role should exist after creation");
 
       // Act
-      Result<?> addResult = roles.permissionRemover().withRole(myRole)
+      Result<?> response = roles.permissionRemover().withRole(myRole)
           .withPermissions(toRemove)
           .run();
-      assertNull("remove-permissions operation error", addResult.getError());
+      assertNull("remove-permissions operation error", response.getError());
 
       // Assert
       assertFalse("should not have permission " + toRemove, checkHasPermission(myRole, toRemove));
     } finally {
       roles.deleter().withName(myRole).run();
-      assertFalse("should not exist after deletion", checkRoleExists(myRole));
+      assertFalse("role should not exist after deletion", checkRoleExists(myRole));
+    }
+  }
+
+  @Test
+  public void testRevokeRole() {
+    String myRole = roleName("VectorOwner");
+    try {
+      // Arrange
+      roles.creator().withName(myRole)
+          .withPermissions(Permission.tenants(TenantsPermission.Action.DELETE))
+          .run();
+      assumeTrue(checkRoleExists(myRole), "role should exist after creation");
+
+      roles.assigner().withUser(adminUser).witRoles(myRole).run();
+      assumeTrue(checkHasRole(adminUser, myRole), adminUser + " should have the assigned role");
+
+      // Act
+      Result<?> response = roles.revoker().withUser(adminUser).witRoles(myRole).run();
+      assertNull("revoke operation error", response.getError());
+
+      // Assert
+      assertFalse("should not have " + myRole + "role", checkHasRole(adminUser, myRole));
+    } finally {
+      roles.deleter().withName(myRole).run();
+      assertFalse("role should not exist after deletion", checkRoleExists(myRole));
+    }
+  }
+
+  @Test
+  public void testAssignRole() {
+    String myRole = roleName("VectorOwner");
+    try {
+      // Arrange
+      roles.creator().withName(myRole)
+          .withPermissions(Permission.tenants(TenantsPermission.Action.DELETE))
+          .run();
+      assumeTrue(checkRoleExists(myRole), "role should exist after creation");
+      assumeFalse(checkHasRole(adminUser, myRole), adminUser + " should not have the new role");
+
+      // Act
+      Result<?> response = roles.assigner().withUser(adminUser).witRoles(myRole).run();
+      assertNull("assign operation error", response.getError());
+
+      // Assert
+      assertTrue("should have " + myRole + "role", checkHasRole(adminUser, myRole));
+    } finally {
+      roles.deleter().withName(myRole).run();
+      assertFalse("role should not exist after deletion", checkRoleExists(myRole));
     }
   }
 
@@ -194,5 +250,9 @@ public class ClientRbacTest {
 
   private boolean checkRoleExists(String role) {
     return roles.exists().withName(role).run().getResult();
+  }
+
+  private boolean checkHasRole(String user, String role) {
+    return roles.assignedUsersGetter().withRole(role).run().getResult().contains(user);
   }
 }
