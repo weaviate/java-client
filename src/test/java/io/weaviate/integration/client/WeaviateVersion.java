5f61f4c2cf9c9137c6656bde82b5b92f2e051332
dyma solovei <dmytro.y.solovei@gmail.com>
2025-01-23 18:21:45 +0100

    ci: fix expected Weaviate server version

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -6,7 +6,7 @@ public class WeaviateVersion {
   public static final String WEAVIATE_IMAGE = "1.28.3-f73fcee";
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.28.3-f73fcee";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.28.3";
   // to be set according to weaviate docker image
   public static final String EXPECTED_WEAVIATE_GIT_HASH = "34bb9ae";
 
