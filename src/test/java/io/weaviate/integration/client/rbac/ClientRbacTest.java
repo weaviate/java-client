59b9a92885464eaed606f0e83dbd53416137df9d
dyma solovei <dmytro.y.solovei@gmail.com>
2025-01-24 16:16:30 +0100

    test: refactor integration test suite to run parametrized tests for sync/async client

--- src/test/java/io/weaviate/integration/client/rbac/ClientRbacTest.java
+++ src/test/java/io/weaviate/integration/client/rbac/ClientRbacTest.java
@@ -1,249 +1,89 @@
 package io.weaviate.integration.client.rbac;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-
 import java.util.List;
-
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.rules.TestName;
 
 import io.weaviate.client.Config;
 import io.weaviate.client.WeaviateAuthClient;
 import io.weaviate.client.base.Result;
 import io.weaviate.client.v1.auth.exception.AuthException;
 import io.weaviate.client.v1.rbac.Roles;
-import io.weaviate.client.v1.rbac.model.BackupsPermission;
-import io.weaviate.client.v1.rbac.model.ClusterPermission;
-import io.weaviate.client.v1.rbac.model.CollectionsPermission;
-import io.weaviate.client.v1.rbac.model.DataPermission;
-import io.weaviate.client.v1.rbac.model.NodesPermission;
-import io.weaviate.client.v1.rbac.model.NodesPermission.Verbosity;
 import io.weaviate.client.v1.rbac.model.Permission;
 import io.weaviate.client.v1.rbac.model.Role;
-import io.weaviate.client.v1.rbac.model.RolesPermission;
-import io.weaviate.client.v1.rbac.model.TenantsPermission;
-import io.weaviate.integration.client.WeaviateDockerCompose;
-import io.weaviate.integration.client.WeaviateDockerCompose.Weaviate;
+import io.weaviate.integration.tests.rbac.ClientRbacTestSuite;
 
-public class ClientRbacTest {
-  private static final String adminRole = "admin";
-  private static final String viewerRole = "viewer";
-  private static final String adminUser = "john-doe";
-
+public class ClientRbacTest implements ClientRbacTestSuite.Rbac {
   private Roles roles;
 
-  @Rule
-  public TestName currentTest = new TestName();
-
-  @ClassRule
-  public static WeaviateDockerCompose compose = WeaviateDockerCompose.rbac(adminUser);
-
-  @Before
-  public void before() throws AuthException {
-    Config config = new Config("http", compose.getHttpHostAddress());
-    roles = WeaviateAuthClient.apiKey(config, Weaviate.makeSecret(adminUser)).roles();
-  }
-
-  public static Object[][] rolesToCreate() {
-    return new Object[][] {
-    };
-  }
-
-  /**
-   * By default the admin user which we use to run the tests
-   * will have 'admin' and 'viewer' roles.
-   */
-  @Test
-  public void testGetAll() {
-    Result<List<Role>> response = roles.allGetter().run();
-    List<Role> all = response.getResult();
-
-    assertThat(response.getError()).as("get all roles error").isNull();
-    assertThat(all).hasSize(2).as("wrong number of roles");
-    assertThat(all.get(0)).returns(adminRole, Role::getName);
-    assertThat(all.get(1)).returns(viewerRole, Role::getName);
-  }
-
-  @Test
-  public void testGetUserRoles() {
-    Result<List<Role>> responseCurrent = roles.userRolesGetter().run();
-    assertThat(responseCurrent.getError()).as("get roles for current user error").isNull();
-    Result<List<Role>> responseAdminUser = roles.userRolesGetter().withUser(adminUser).run();
-    assertThat(responseAdminUser.getError()).as("get roles for user error").isNull();
-
-    List<Role> currentRoles = responseCurrent.getResult();
-    List<Role> adminRoles = responseAdminUser.getResult();
-
-    Assertions.assertArrayEquals(currentRoles.toArray(), adminRoles.toArray(), "expect same set of roles");
-  }
-
-  public void testGetAssignedUsers() {
-    Result<List<String>> response = roles.assignedUsersGetter().withRole(adminRole).run();
-    assertThat(response.getError()).as("get assigned users error").isNull();
-
-    List<String> users = response.getResult();
-    assertThat(users).as("users assigned to " + adminRole + " role").hasSize(1);
-    assertEquals(adminUser, users.get(0), "wrong user assinged to " + adminRole + " role");
-  }
-
-  // TODO: check if I can create a role with a name that's not a valid URL
-  // paramter
-
-  @Test
-  public void testCreate() {
-    String myRole = roleName("VectorOwner");
-    String myCollection = "Pizza";
-
-    Permission<?>[] wantPermissions = new Permission<?>[] {
-        Permission.backups(BackupsPermission.Action.MANAGE, myCollection),
-        Permission.cluster(ClusterPermission.Action.READ),
-        Permission.nodes(NodesPermission.Action.READ, Verbosity.MINIMAL, myCollection),
-        Permission.roles(RolesPermission.Action.MANAGE, viewerRole),
-        Permission.collections(CollectionsPermission.Action.CREATE, myCollection),
-        Permission.data(DataPermission.Action.UPDATE, myCollection),
-        Permission.tenants(TenantsPermission.Action.DELETE),
-    };
-
+  public ClientRbacTest(Config config, String apiKey) {
     try {
-      // Arrange
-      deleteRole(myRole);
-
-      // Act
-      createRole(myRole, wantPermissions);
-
-      Result<Role> response = roles.getter().withName(myRole).run();
-      Role role = response.getResult();
-      assertNull("error fetching a role", response.getError());
-      assertThat(role).as("wrong role name").returns(myRole, Role::getName);
-
-      for (int i = 0; i < wantPermissions.length; i++) {
-        Permission<?> perm = wantPermissions[i];
-        assertTrue("should have permission " + perm, checkHasPermission(myRole, perm));
-      }
-    } finally {
-      deleteRole(myRole);
+      this.roles = WeaviateAuthClient.apiKey(config, apiKey).roles();
+    } catch (AuthException e) {
+      throw new RuntimeException(e);
     }
   }
 
-  @Test
-  public void testAddPermissions() {
-    String myRole = roleName("VectorOwner");
-    Permission<?> toAdd = Permission.cluster(ClusterPermission.Action.READ);
-    try {
-      // Arrange
-      createRole(myRole, Permission.tenants(TenantsPermission.Action.DELETE));
-
-      // Act
-      Result<?> response = roles.permissionAdder().withRole(myRole)
-          .withPermissions(toAdd)
-          .run();
-      assertNull("add-permissions operation error", response.getError());
-
-      // Assert
-      assertTrue("should have permission " + toAdd, checkHasPermission(myRole, toAdd));
-    } finally {
-      deleteRole(myRole);
-    }
+  @Override
+  public Result<Role> getRole(String role) {
+    return roles.getter().withName(role).run();
   }
 
-  @Test
-  public void testRemovePermissions() {
-    String myRole = roleName("VectorOwner");
-    Permission<?> toRemove = Permission.tenants(TenantsPermission.Action.DELETE);
-    try {
-      // Arrange
-      createRole(myRole,
-          Permission.cluster(ClusterPermission.Action.READ),
-          Permission.tenants(TenantsPermission.Action.DELETE));
-
-      // Act
-      Result<?> response = roles.permissionRemover().withRole(myRole)
-          .withPermissions(toRemove)
-          .run();
-      assertNull("remove-permissions operation error", response.getError());
-
-      // Assert
-      assertFalse("should not have permission " + toRemove, checkHasPermission(myRole, toRemove));
-    } finally {
-      deleteRole(myRole);
-    }
+  @Override
+  public Result<List<Role>> getAll() {
+    return roles.allGetter().run();
   }
 
-  @Test
-  public void testRevokeRole() {
-    String myRole = roleName("VectorOwner");
-    try {
-      // Arrange
-      createRole(myRole, Permission.tenants(TenantsPermission.Action.DELETE));
-      roles.assigner().withUser(adminUser).witRoles(myRole).run();
-      assumeTrue(checkHasRole(adminUser, myRole), adminUser + " should have the assigned role");
-
-      // Act
-      Result<?> response = roles.revoker().withUser(adminUser).witRoles(myRole).run();
-      assertNull("revoke operation error", response.getError());
-
-      // Assert
-      assertFalse("should not have " + myRole + "role", checkHasRole(adminUser, myRole));
-    } finally {
-      deleteRole(myRole);
-    }
+  @Override
+  public Result<List<Role>> getUserRoles() {
+    return roles.userRolesGetter().run();
   }
 
-  @Test
-  public void testAssignRole() {
-    String myRole = roleName("VectorOwner");
-    try {
-      // Arrange
-      createRole(myRole, Permission.tenants(TenantsPermission.Action.DELETE));
-      assumeFalse(checkHasRole(adminUser, myRole), adminUser + " should not have the new role");
-
-      // Act
-      Result<?> response = roles.assigner().withUser(adminUser).witRoles(myRole).run();
-      assertNull("assign operation error", response.getError());
-
-      // Assert
-      assertTrue("should have " + myRole + "role", checkHasRole(adminUser, myRole));
-    } finally {
-      deleteRole(myRole);
-    }
+  @Override
+  public Result<List<Role>> getUserRoles(String user) {
+    return roles.userRolesGetter().withUser(user).run();
   }
 
-  /** Prefix the role with the name of the current test for easier debugging */
-  private String roleName(String name) {
-    return String.format("%s-%s", currentTest.getMethodName(), name);
+  @Override
+  public Result<List<String>> getAssignedUsers(String role) {
+    return roles.assignedUsersGetter().withRole(role).run();
   }
 
-  private boolean checkHasPermission(String role, Permission<? extends Permission<?>> perm) {
-    return roles.permissionChecker().withRole(role).withPermission(perm).run().getResult();
-  }
-
-  private boolean checkRoleExists(String role) {
-    return roles.exists().withName(role).run().getResult();
-  }
-
-  private boolean checkHasRole(String user, String role) {
-    return roles.assignedUsersGetter().withRole(role).run().getResult().contains(user);
-  }
-
-  private void createRole(String role, Permission<?>... permissions) {
+  @Override
+  public void createRole(String role, Permission<?>... permissions) {
     roles.creator().withName(role).withPermissions(permissions).run();
-    assumeTrue(checkRoleExists(role), "role should exist after creation");
-
   }
 
-  private void deleteRole(String role) {
+  @Override
+  public void deleteRole(String role) {
     roles.deleter().withName(role).run();
-    assertFalse("role should not exist after deletion", checkRoleExists(role));
+  }
 
+  @Override
+  public Result<Boolean> hasPermission(String role, Permission<?> perm) {
+    return roles.permissionChecker().withRole(role).withPermission(perm).run();
+  }
+
+  @Override
+  public Result<Boolean> exists(String role) {
+    return roles.exists().withName(role).run();
+  }
+
+  @Override
+  public Result<?> addPermissions(String role, Permission<?>... permissions) {
+    return roles.permissionAdder().withRole(role).withPermissions(permissions).run();
+  }
+
+  @Override
+  public Result<?> removePermissions(String role, Permission<?>... permissions) {
+    return roles.permissionRemover().withRole(role).withPermissions(permissions).run();
+  }
+
+  @Override
+  public Result<?> assignRoles(String user, String... roles) {
+    return this.roles.assigner().withUser(user).witRoles(roles).run();
+  }
+
+  @Override
+  public Result<?> revokeRoles(String user, String... roles) {
+    return this.roles.revoker().withUser(user).witRoles(roles).run();
   }
 }
---
+++ src/test/java/io/weaviate/integration/tests/rbac/ClientRbacTestSuite.java
@@ -0,0 +1,286 @@
+package io.weaviate.integration.tests.rbac;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+
+import com.jparams.junit4.JParamsTestRunner;
+import com.jparams.junit4.data.DataMethod;
+
+import io.weaviate.client.Config;
+import io.weaviate.client.base.Result;
+import io.weaviate.client.v1.auth.exception.AuthException;
+import io.weaviate.client.v1.rbac.model.BackupsPermission;
+import io.weaviate.client.v1.rbac.model.ClusterPermission;
+import io.weaviate.client.v1.rbac.model.CollectionsPermission;
+import io.weaviate.client.v1.rbac.model.DataPermission;
+import io.weaviate.client.v1.rbac.model.NodesPermission;
+import io.weaviate.client.v1.rbac.model.NodesPermission.Verbosity;
+import io.weaviate.client.v1.rbac.model.Permission;
+import io.weaviate.client.v1.rbac.model.Role;
+import io.weaviate.client.v1.rbac.model.RolesPermission;
+import io.weaviate.client.v1.rbac.model.TenantsPermission;
+import io.weaviate.integration.client.WeaviateDockerCompose;
+import io.weaviate.integration.client.WeaviateDockerCompose.Weaviate;
+
+@RunWith(JParamsTestRunner.class)
+public class ClientRbacTestSuite {
+
+  private static final String adminRole = "admin";
+  private static final String viewerRole = "viewer";
+  private static final String adminUser = "john-doe";
+  private static final String API_KEY = Weaviate.makeSecret(adminUser);
+
+  @Rule
+  public TestName currentTest = new TestName();
+
+  @ClassRule
+  public static WeaviateDockerCompose compose = WeaviateDockerCompose.rbac(adminUser);
+
+  public static Config config() {
+    return new Config("http", compose.getHttpHostAddress());
+  }
+
+  public static Object[][] clients() {
+    try {
+      Object[][] r = new Object[][] {
+          {
+              (Supplier<Rbac>) () -> new io.weaviate.integration.client.rbac.ClientRbacTest(config(), API_KEY) },
+      };
+      return r;
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return null;
+    }
+  }
+
+  /**
+   * By default the admin user which we use to run the tests
+   * will have 'admin' and 'viewer' roles.
+   */
+  @DataMethod(source = ClientRbacTestSuite.class, method = "clients")
+  @Test
+  public void testGetAll(Supplier<Rbac> rbac) {
+    Rbac roles = rbac.get();
+    Result<List<Role>> response = roles.getAll();
+    List<Role> all = response.getResult();
+
+    assertThat(response.getError()).as("get all roles error").isNull();
+    assertThat(all).hasSize(2).as("wrong number of roles");
+    assertThat(all.get(0)).returns(adminRole, Role::getName);
+    assertThat(all.get(1)).returns(viewerRole, Role::getName);
+  }
+
+  /**
+   * Roles retrieved for "current user" should be identical to the ones
+   * retrieved for them explicitly (by passing the username).
+   */
+  @DataMethod(source = ClientRbacTestSuite.class, method = "clients")
+  @Test
+  public void testGetUserRoles(Supplier<Rbac> rbac) {
+    Rbac roles = rbac.get();
+    Result<List<Role>> responseCurrent = roles.getUserRoles();
+    assertThat(responseCurrent.getError()).as("get roles for current user error").isNull();
+    Result<List<Role>> responseAdminUser = roles.getUserRoles(adminUser);
+    assertThat(responseAdminUser.getError()).as("get roles for user error").isNull();
+
+    List<Role> currentRoles = responseCurrent.getResult();
+    List<Role> adminRoles = responseAdminUser.getResult();
+
+    Assertions.assertArrayEquals(currentRoles.toArray(), adminRoles.toArray(), "expect same set of roles");
+  }
+
+  /** Admin user should have the admin role assigned to them. */
+  public void testGetAssignedUsers(Supplier<Rbac> rbac) {
+    Rbac roles = rbac.get();
+    Result<List<String>> response = roles.getAssignedUsers(adminRole);
+    assertThat(response.getError()).as("get assigned users error").isNull();
+
+    List<String> users = response.getResult();
+    assertThat(users).as("users assigned to " + adminRole + " role").hasSize(1);
+    assertEquals(adminUser, users.get(0), "wrong user assinged to " + adminRole + " role");
+  }
+
+  // TODO: check if I can create a role with a name that's not a valid URL
+  // paramter
+
+  /**
+   * Created role should have all of the permissions it was created with.
+   * Tests addition and fetching the role to.
+   */
+  @DataMethod(source = ClientRbacTestSuite.class, method = "clients")
+  @Test
+  public void testCreate(Supplier<Rbac> rbac) {
+    Rbac roles = rbac.get();
+    String myRole = roleName("VectorOwner");
+    String myCollection = "Pizza";
+
+    Permission<?>[] wantPermissions = new Permission<?>[] {
+        Permission.backups(BackupsPermission.Action.MANAGE, myCollection),
+        Permission.cluster(ClusterPermission.Action.READ),
+        Permission.nodes(NodesPermission.Action.READ, Verbosity.MINIMAL, myCollection),
+        Permission.roles(RolesPermission.Action.MANAGE, viewerRole),
+        Permission.collections(CollectionsPermission.Action.CREATE, myCollection),
+        Permission.data(DataPermission.Action.UPDATE, myCollection),
+        Permission.tenants(TenantsPermission.Action.DELETE),
+    };
+
+    try {
+      // Arrange
+      roles.deleteRole(myRole);
+
+      // Act
+      roles.createRole(myRole, wantPermissions);
+
+      Result<Role> response = roles.getRole(myRole);
+      Role role = response.getResult();
+      assertNull("error fetching a role", response.getError());
+      assertThat(role).as("wrong role name").returns(myRole, Role::getName);
+
+      for (int i = 0; i < wantPermissions.length; i++) {
+        Permission<?> perm = wantPermissions[i];
+        assertTrue("should have permission " + perm, checkHasPermission(roles, myRole, perm));
+      }
+    } finally {
+      roles.deleteRole(myRole);
+    }
+  }
+
+  /**
+   * Role can be extended with new permissions. We do not test the "upsert"
+   * behavior because it is the server's responsibility.
+   */
+  @DataMethod(source = ClientRbacTestSuite.class, method = "clients")
+  @Test
+  public void testAddPermissions(Supplier<Rbac> rbac) {
+    Rbac roles = rbac.get();
+    String myRole = roleName("VectorOwner");
+    Permission<?> toAdd = Permission.cluster(ClusterPermission.Action.READ);
+    try {
+      // Arrange
+      roles.createRole(myRole, Permission.tenants(TenantsPermission.Action.DELETE));
+
+      // Act
+      Result<?> response = roles.addPermissions(myRole, toAdd);
+      assertNull("add-permissions operation error", response.getError());
+
+      // Assert
+      assertTrue("should have permission " + toAdd, checkHasPermission(roles, myRole, toAdd));
+    } finally {
+      roles.deleteRole(myRole);
+    }
+  }
+
+  /**
+   * Permissions can be removed from a role.
+   * We do not test the "downsert" behavior, because it is the server's
+   * responsibility.
+   */
+  @DataMethod(source = ClientRbacTestSuite.class, method = "clients")
+  @Test
+  public void testRemovePermissions(Supplier<Rbac> rbac) {
+    Rbac roles = rbac.get();
+    String myRole = roleName("VectorOwner");
+    Permission<?> toRemove = Permission.tenants(TenantsPermission.Action.DELETE);
+    try {
+      // Arrange
+      roles.createRole(myRole,
+          Permission.cluster(ClusterPermission.Action.READ),
+          Permission.tenants(TenantsPermission.Action.DELETE));
+
+      // Act
+      Result<?> response = roles.removePermissions(myRole, toRemove);
+      assertNull("remove-permissions operation error", response.getError());
+
+      // Assert
+      assertFalse("should not have permission " + toRemove, checkHasPermission(roles, myRole, toRemove));
+    } finally {
+      roles.deleteRole(myRole);
+    }
+  }
+
+  /** User can be assigned a role and the role can be revoked. */
+  @DataMethod(source = ClientRbacTestSuite.class, method = "clients")
+  @Test
+  public void testAssignRevokeRole(Supplier<Rbac> rbac) {
+    Rbac roles = rbac.get();
+    String myRole = roleName("VectorOwner");
+    try {
+      // Arrange
+      roles.createRole(myRole, Permission.tenants(TenantsPermission.Action.DELETE));
+
+      // Act: Assign
+      roles.assignRoles(adminUser, myRole);
+      assumeTrue(checkHasRole(roles, adminUser, myRole), adminUser + " should have the assigned role");
+
+      // Act: Revoke
+      Result<?> response = roles.revokeRoles(adminUser, myRole);
+      assertNull("revoke operation error", response.getError());
+
+      // Assert
+      assertFalse("should not have " + myRole + " role", checkHasRole(roles, adminUser, myRole));
+    } finally {
+      roles.deleteRole(myRole);
+    }
+  }
+
+  /** Prefix the role with the name of the current test for easier debugging */
+  private String roleName(String name) {
+    return String.format("%s-%s", currentTest.getMethodName(), name);
+  }
+
+  private boolean checkHasPermission(Rbac roles, String role, Permission<? extends Permission<?>> perm) {
+    return roles.hasPermission(role, perm).getResult();
+  }
+
+  private boolean checkHasRole(Rbac roles, String user, String role) {
+    return roles.getAssignedUsers(role).getResult().contains(user);
+  }
+
+  /**
+   * Sync and async test suits should provide an implementation of this interface.
+   * This way the test suite can be written once with very little
+   * boilerplate/overhead.
+   */
+  public interface Rbac {
+    Result<Role> getRole(String role);
+
+    Result<List<Role>> getAll();
+
+    Result<List<Role>> getUserRoles();
+
+    Result<List<Role>> getUserRoles(String user);
+
+    Result<List<String>> getAssignedUsers(String role);
+
+    void createRole(String role, Permission<?>... permissions);
+
+    void deleteRole(String role);
+
+    Result<Boolean> hasPermission(String role, Permission<?> perm);
+
+    Result<Boolean> exists(String role);
+
+    Result<?> addPermissions(String role, Permission<?>... permissions);
+
+    Result<?> removePermissions(String role, Permission<?>... permissions);
+
+    Result<?> assignRoles(String user, String... roles);
+
+    Result<?> revokeRoles(String user, String... roles);
+  }
+
+}
