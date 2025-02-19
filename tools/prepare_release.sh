0592ec5131eed0532a02b14f31b201870d92cd5b
Marcin Antas <antas.marcin@gmail.com>
2025-02-19 14:02:33 +0100

    Add support for alpha releases

--- tools/prepare_release.sh
+++ tools/prepare_release.sh
@@ -22,6 +22,11 @@ if git rev-parse "$VERSION" >/dev/null 2>&1; then
   exit 1
 fi
 
+next_version=""
+if [[ "$VERSION" =~ "alpha" ]]; then
+  next_version=$(echo "$VERSION" | sed 's/-.*//')
+fi
+
 mvn versions:set -DnewVersion=$VERSION versions:commit
 sed -i '' "s/^\([[:blank:]]*\)<tag>.*/\1<tag>$VERSION<\/tag>/" pom.xml
 sed -i '' "s/^\([[:blank:]]*\)<version>.*/\1<version>$VERSION<\/version>/" README.md
@@ -29,6 +34,10 @@ sed -i '' "s/^\([[:blank:]]*\)<version>.*/\1<version>$VERSION<\/version>/" READM
 git commit -a -m "Release $VERSION version"
 git tag -a "$VERSION" -m "$VERSION"
 
-mvn versions:set -DnextSnapshot=true versions:commit
+if [[ "$next_version" != "" ]]; then
+  mvn versions:set -DnewVersion="$next_version-SNAPSHOT" versions:commit
+else
+  mvn versions:set -DnextSnapshot=true versions:commit
+fi
 
 git commit -a -m "Update version to next snapshot version"
