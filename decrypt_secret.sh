94605a350442d71c8bb0549599ee1c1a892880a7
Marcin Antas <antas.marcin@gmail.com>
2025-02-20 22:04:30 +0100

    Update gpg file, release pipeline and decrypt shell script

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output secrets.tar secrets.tar.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" --output secrets.tar secrets.tar.gpg
 tar xvf secrets.tar
 gpg --batch --import key.gpg
-GPG_TTY="$(tty)"
-export GPG_TTY
+gpg --list-secret-keys
+echo "Success"
