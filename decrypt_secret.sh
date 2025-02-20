c9c3a5ffe1fe02cfeb4b236c930ef6fdddac188e
Marcin Antas <antas.marcin@gmail.com>
2025-02-20 15:16:47 +0100

    Update gpg file and decrypt.sh script

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output secrets.tar secrets.tar.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" --output secrets.tar secrets.tar.gpg
 tar xvf secrets.tar
 gpg --batch --import key.gpg
 GPG_TTY="$(tty)"
