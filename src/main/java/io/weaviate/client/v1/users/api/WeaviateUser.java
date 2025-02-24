1d80eb5952629fb6ed663375e0a2299fad892bce
dyma solovei <dyma@weaviate.io>
2025-02-24 18:11:54 +0100

    feat: read username into userId field if user_id not available

--- src/main/java/io/weaviate/client/v1/users/api/WeaviateUser.java
+++ src/main/java/io/weaviate/client/v1/users/api/WeaviateUser.java
@@ -1,17 +1,26 @@
 package io.weaviate.client.v1.users.api;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.google.gson.annotations.SerializedName;
 
 import io.weaviate.client.v1.rbac.api.WeaviateRole;
 import io.weaviate.client.v1.users.model.User;
 
 public class WeaviateUser {
-  String name;
+  @SerializedName("username")
+  String username;
+
+  @SerializedName("user_id")
   String id;
-  List<WeaviateRole> roles;
+
+  @SerializedName("roles")
+  List<WeaviateRole> roles = new ArrayList<>();
 
   public User toUser() {
-    return new User(name, id, roles.stream().map(WeaviateRole::toRole).collect(Collectors.toList()));
+    return new User(id != null ? id : username,
+        roles.stream().map(WeaviateRole::toRole).collect(Collectors.toList()));
   }
 }
