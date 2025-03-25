e76a20d42e5e1be0ff63a32b51b875f89beea5fa
dyma solovei <dyma@weaviate.io>
2025-03-25 14:28:27 +0100

    refactor: move unmarshaling code to internal/codec

--- src/main/java/io/weaviate/client6/v1/collections/aggregate/WeaviateAggregate.java
+++ src/main/java/io/weaviate/client6/v1/collections/aggregate/WeaviateAggregate.java
@@ -89,12 +89,11 @@ public class WeaviateAggregate {
 
   public AggregateGroupByResponse nearVector(
       Float[] vector,
-      Consumer<NearVector.Builder> nearVectorOptions,
-      Consumer<AggregateGroupByRequest.GroupBy.Builder> groupByOptions,
+      AggregateGroupByRequest.GroupBy groupBy,
       Consumer<AggregateRequest.Builder> options) {
     var aggregation = AggregateRequest.with(collectionName, options);
-    var nearVector = NearVector.with(vector, nearVectorOptions);
-    var groupBy = AggregateGroupByRequest.GroupBy.with(groupByOptions);
+    var nearVector = NearVector.with(vector, opt -> {
+    });
 
     var req = new AggregateMarshaler(aggregation.collectionName())
         .addAggregation(aggregation)
@@ -102,49 +101,23 @@ public class WeaviateAggregate {
         .addNearVector(nearVector)
         .marshal();
     var reply = grpcClient.grpc.aggregate(req);
+    return new AggregateUnmarshaler(reply).grouped();
+  }
 
-    List<Group<?>> groups = new ArrayList<>();
-    if (reply.hasGroupedResults()) {
-      for (var result : reply.getGroupedResults().getGroupsList()) {
-        final Long totalCount = result.hasObjectsCount() ? result.getObjectsCount() : null;
+  public AggregateGroupByResponse nearVector(
+      Float[] vector,
+      Consumer<NearVector.Builder> nearVectorOptions,
+      AggregateGroupByRequest.GroupBy groupBy,
+      Consumer<AggregateRequest.Builder> options) {
+    var aggregation = AggregateRequest.with(collectionName, options);
+    var nearVector = NearVector.with(vector, nearVectorOptions);
 
-        GroupedBy<?> groupedBy = null;
-        var gb = result.getGroupedBy();
-        if (gb.hasInt()) {
-          groupedBy = new GroupedBy<Long>(gb.getPathList().get(0), gb.getInt());
-        } else if (gb.hasText()) {
-          groupedBy = new GroupedBy<String>(gb.getPathList().get(0), gb.getText());
-        } else {
-          assert false : "branch not covered";
-        }
-
-        Map<String, Metric.Values> properties = new HashMap<>();
-        for (var agg : result.getAggregations().getAggregationsList()) {
-          var property = agg.getProperty();
-          Metric.Values value = null;
-
-          if (agg.hasInt()) {
-            var metrics = agg.getInt();
-            value = new IntegerMetric.Values(
-                metrics.hasCount() ? metrics.getCount() : null,
-                metrics.hasMinimum() ? metrics.getMinimum() : null,
-                metrics.hasMaximum() ? metrics.getMaximum() : null,
-                metrics.hasMean() ? metrics.getMean() : null,
-                metrics.hasMedian() ? metrics.getMedian() : null,
-                metrics.hasMode() ? metrics.getMode() : null,
-                metrics.hasSum() ? metrics.getSum() : null);
-          } else {
-            assert false : "branch not covered";
-          }
-          if (value != null) {
-            properties.put(property, value);
-          }
-        }
-        Group<?> group = new Group<>(groupedBy, properties, totalCount);
-        groups.add(group);
-
-      }
-    }
-    return new AggregateGroupByResponse(groups);
+    var req = new AggregateMarshaler(aggregation.collectionName())
+        .addAggregation(aggregation)
+        .addGroupBy(groupBy)
+        .addNearVector(nearVector)
+        .marshal();
+    var reply = grpcClient.grpc.aggregate(req);
+    return new AggregateUnmarshaler(reply).grouped();
   }
 }
