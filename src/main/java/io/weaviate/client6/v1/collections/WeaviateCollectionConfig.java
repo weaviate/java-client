0448e4ff21a616fce26e3d0d58ecf2d83233c19a
dyma solovei <dyma@weaviate.io>
2025-03-26 14:08:56 +0100

    feat: create reference properties + add references to a collection

---
+++ src/main/java/io/weaviate/client6/v1/collections/WeaviateCollectionConfig.java
@@ -0,0 +1,68 @@
+package io.weaviate.client6.v1.collections;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
+
+import com.google.gson.Gson;
+
+import io.weaviate.client6.Config;
+import io.weaviate.client6.internal.DtoTypeAdapterFactory;
+import io.weaviate.client6.internal.HttpClient;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class WeaviateCollectionConfig {
+  // TODO: hide befind an internal HttpClient
+  private final String collectionName;
+  private final Config config;
+  private final HttpClient httpClient;
+
+  private static final Gson gson = new Gson();
+  static {
+    DtoTypeAdapterFactory.register(
+        CollectionDefinition.class,
+        CollectionDefinitionDTO.class,
+        m -> {
+          return new CollectionDefinitionDTO(m);
+        });
+  }
+
+  public Optional<CollectionDefinition> get() throws IOException {
+    ClassicHttpRequest httpGet = ClassicRequestBuilder
+        .get(config.baseUrl() + "/schema/" + collectionName)
+        .build();
+
+    return httpClient.http.execute(httpGet, response -> {
+      if (response.getCode() == HttpStatus.SC_NOT_FOUND) {
+        return Optional.empty();
+      }
+      try (var r = new InputStreamReader(response.getEntity().getContent())) {
+        var collection = gson.fromJson(r, CollectionDefinition.class);
+        return Optional.ofNullable(collection);
+      }
+    });
+  }
+
+  public void addProperty(Property property) throws IOException {
+    ClassicHttpRequest httpPost = ClassicRequestBuilder
+        .post(config.baseUrl() + "/schema/" + collectionName + "/properties")
+        .setEntity(gson.toJson(property), ContentType.APPLICATION_JSON)
+        .build();
+
+    httpClient.http.execute(httpPost, response -> {
+      var entity = response.getEntity();
+      if (response.getCode() != HttpStatus.SC_SUCCESS) {
+        var message = EntityUtils.toString(entity);
+        throw new RuntimeException("HTTP " + response.getCode() + ": " + message);
+      }
+      return null;
+    });
+  }
+}
