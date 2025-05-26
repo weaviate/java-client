599664165b7c72def65c99be8ae8c5f349c8a130
dyma solovei <dyma@weaviate.io>
2025-05-26 17:09:28 +0200

    feat: delegate factory for dto/model conversion

---
+++ src/main/java/io/weaviate/client6/v1/internal/json/DelegateJson.java
@@ -0,0 +1,12 @@
+package io.weaviate.client6.v1.internal.json;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface DelegateJson {
+  Class<? extends JsonDelegate<?>> value();
+}
