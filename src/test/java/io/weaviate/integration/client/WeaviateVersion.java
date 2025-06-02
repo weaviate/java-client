628ed1d97dd68e3ac3bc117b147eb91a606b9479
dyma solovei <dyma@weaviate.io>
2025-06-02 17:24:01 +0200

    ci: update expected git hash

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -8,7 +8,7 @@ public class WeaviateVersion {
   // to be set according to weaviate docker image
   public static final String EXPECTED_WEAVIATE_VERSION = "1.31.0";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "5ff7495";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "79499d6";
 
   private WeaviateVersion() {
   }
