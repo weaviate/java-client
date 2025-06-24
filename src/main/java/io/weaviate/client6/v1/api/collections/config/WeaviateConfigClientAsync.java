48f15b3f9a44f1655600092dc286da4a5eb874d4
dyma solovei <dyma@weaviate.io>
2025-06-24 19:36:17 +0200

    feat(wip): add config.update

--- src/main/java/io/weaviate/client6/v1/api/collections/config/WeaviateConfigClientAsync.java
+++ src/main/java/io/weaviate/client6/v1/api/collections/config/WeaviateConfigClientAsync.java
@@ -3,23 +3,25 @@ package io.weaviate.client6.v1.api.collections.config;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 import io.weaviate.client6.v1.api.collections.Property;
 import io.weaviate.client6.v1.api.collections.WeaviateCollection;
 import io.weaviate.client6.v1.api.collections.WeaviateCollectionsClientAsync;
+import io.weaviate.client6.v1.internal.ObjectBuilder;
 import io.weaviate.client6.v1.internal.grpc.GrpcTransport;
 import io.weaviate.client6.v1.internal.orm.CollectionDescriptor;
 import io.weaviate.client6.v1.internal.rest.RestTransport;
 
 public class WeaviateConfigClientAsync {
-  private final RestTransport transport;
+  private final RestTransport restTransport;
   private final WeaviateCollectionsClientAsync collectionsClient;
 
   protected final CollectionDescriptor<?> collection;
 
   public WeaviateConfigClientAsync(CollectionDescriptor<?> collection, RestTransport restTransport,
       GrpcTransport grpcTransport) {
-    this.transport = restTransport;
+    this.restTransport = restTransport;
     this.collectionsClient = new WeaviateCollectionsClientAsync(restTransport, grpcTransport);
 
     this.collection = collection;
@@ -30,11 +32,17 @@ public class WeaviateConfigClientAsync {
   }
 
   public CompletableFuture<Void> addProperty(Property property) throws IOException {
-    return this.transport.performRequestAsync(new AddPropertyRequest(collection.name(), property),
+    return this.restTransport.performRequestAsync(new AddPropertyRequest(collection.name(), property),
         AddPropertyRequest._ENDPOINT);
   }
 
   public CompletableFuture<Void> addReference(String name, String... dataTypes) throws IOException {
     return this.addProperty(Property.reference(name, dataTypes).toProperty());
+  }
+
+  public CompletableFuture<Void> update(String collectionName,
+      Function<UpdateCollectionRequest.Builder, ObjectBuilder<UpdateCollectionRequest>> fn) throws IOException {
+    return this.restTransport.performRequestAsync(UpdateCollectionRequest.of(collectionName, fn),
+        UpdateCollectionRequest._ENDPOINT);
   }
 }
