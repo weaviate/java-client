eb7d5a2f12d081438eecded23774290db548f6dc
dyma solovei <dyma@weaviate.io>
2025-06-27 17:26:24 +0200

    feat: stream() and list() objects in collection
    CursorSpliterator powers 2 patterns for iterating over objects:
    - list() returns an Iterable that can be used in a for-loop
    - stream() presents the internal Iterator via a familiar Stream API

--- src/main/java/io/weaviate/client6/v1/api/collections/CollectionHandle.java
+++ src/main/java/io/weaviate/client6/v1/api/collections/CollectionHandle.java
@@ -1,8 +1,14 @@
 package io.weaviate.client6.v1.api.collections;
 
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 import io.weaviate.client6.v1.api.collections.aggregate.WeaviateAggregateClient;
 import io.weaviate.client6.v1.api.collections.config.WeaviateConfigClient;
 import io.weaviate.client6.v1.api.collections.data.WeaviateDataClient;
+import io.weaviate.client6.v1.api.collections.query.QueryMetadata;
 import io.weaviate.client6.v1.api.collections.query.WeaviateQueryClient;
 import io.weaviate.client6.v1.internal.grpc.GrpcTransport;
 import io.weaviate.client6.v1.internal.orm.CollectionDescriptor;
@@ -23,5 +29,19 @@ public class CollectionHandle<T> {
     this.query = new WeaviateQueryClient<>(collectionDescriptor, grpcTransport);
     this.data = new WeaviateDataClient<>(collectionDescriptor, restTransport, this.query);
     this.aggregate = new WeaviateAggregateClient(collectionDescriptor, grpcTransport);
+  }
+
+  public Stream<WeaviateObject<T, Object, QueryMetadata>> stream() {
+    return StreamSupport.stream(spliterator(2), false);
+  }
+
+  public Iterable<WeaviateObject<T, Object, QueryMetadata>> list() {
+    return () -> Spliterators.iterator(spliterator(2));
+  }
+
+  private Spliterator<WeaviateObject<T, Object, QueryMetadata>> spliterator(int batchSize) {
+    return new CursorSpliterator<>(batchSize,
+        (after, limit) -> this.query.fetchObjects(
+            query -> query.after(after).limit(limit)).objects());
   }
 }
