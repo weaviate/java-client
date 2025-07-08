4d016129bdc2fce65bde7d708e436b2f8952176d
Roberto Esposito <robyesposito2000@gmail.com>
2025-07-08 18:49:01 -0400

    update weaviate version

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -3,12 +3,12 @@ package io.weaviate.integration.client;
 public class WeaviateVersion {
 
   // docker image version
-  public static final String WEAVIATE_IMAGE = "1.32.0-dev-c2684c5";
+  public static final String WEAVIATE_IMAGE = "1.32.0-rc.1-175d482";
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.32.0-dev";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.32.0-rc.1";
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_GIT_HASH = "c2684c5";
+  public static final String EXPECTED_WEAVIATE_GIT_HASH = "175d482";
 
   private WeaviateVersion() {}
 }
