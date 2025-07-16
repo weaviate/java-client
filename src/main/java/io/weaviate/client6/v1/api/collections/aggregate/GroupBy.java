af834543cdbfec9825e63df871461c2569672052
dyma solovei <dyma@weaviate.io>
2025-07-16 16:16:58 +0200

    feat: expose limit parameter for grouped Aggregate request

--- src/main/java/io/weaviate/client6/v1/api/collections/aggregate/GroupBy.java
+++ src/main/java/io/weaviate/client6/v1/api/collections/aggregate/GroupBy.java
@@ -1,13 +1,48 @@
 package io.weaviate.client6.v1.api.collections.aggregate;
 
+import java.util.function.Function;
+
+import io.weaviate.client6.v1.internal.ObjectBuilder;
 import io.weaviate.client6.v1.internal.grpc.protocol.WeaviateProtoAggregate;
 
-public record GroupBy(String property) {
-  public static final GroupBy of(String property) {
-    return new GroupBy(property);
+public record GroupBy(String property, Integer limit) {
+  public static final GroupBy property(String property) {
+    return property(property, ObjectBuilder.identity());
+  }
+
+  public static final GroupBy property(String property, Function<Builder, ObjectBuilder<GroupBy>> fn) {
+    return fn.apply(new Builder(property)).build();
+  }
+
+  public GroupBy(Builder builder) {
+    this(builder.property, builder.limit);
+  }
+
+  public static class Builder implements ObjectBuilder<GroupBy> {
+    private final String property;
+
+    public Builder(String property) {
+      this.property = property;
+    }
+
+    private Integer limit;
+
+    public final Builder limit(int limit) {
+      this.limit = limit;
+      return this;
+    }
+
+    @Override
+    public GroupBy build() {
+      return new GroupBy(this);
+    }
   }
 
   void appendTo(WeaviateProtoAggregate.AggregateRequest.Builder req, String collection) {
+    if (limit != null) {
+      req.setLimit(limit);
+    }
+
     req.setGroupBy(WeaviateProtoAggregate.AggregateRequest.GroupBy.newBuilder()
         .setCollection(collection)
         .setProperty(property));
