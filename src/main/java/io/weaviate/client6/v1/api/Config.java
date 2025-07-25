54ab99c985a8311855242703d44192291668db6f
dyma solovei <dyma@weaviate.io>
2025-07-25 14:57:07 +0200

    docs: add javadoc for client config

--- src/main/java/io/weaviate/client6/v1/api/Config.java
+++ src/main/java/io/weaviate/client6/v1/api/Config.java
@@ -38,11 +38,11 @@ public record Config(
         builder.trustManagerFactory);
   }
 
-  public RestTransportOptions restTransportOptions() {
+  RestTransportOptions restTransportOptions() {
     return new RestTransportOptions(scheme, httpHost, httpPort, headers, tokenProvider, trustManagerFactory);
   }
 
-  public GrpcChannelOptions grpcTransportOptions() {
+  GrpcChannelOptions grpcTransportOptions() {
     return new GrpcChannelOptions(scheme, grpcHost, grpcPort, headers, tokenProvider, trustManagerFactory);
   }
 
@@ -57,18 +57,30 @@ public record Config(
     protected TrustManagerFactory trustManagerFactory;
     protected Map<String, String> headers = new HashMap<>();
 
+    /**
+     * Set URL scheme. Subclasses may increase the visibility of this method to
+     * {@code public} if using a different scheme is allowed.
+     */
     @SuppressWarnings("unchecked")
     protected SELF scheme(String scheme) {
       this.scheme = scheme;
       return (SELF) this;
     }
 
+    /**
+     * Set port for REST requests. Subclasses may increase the visibility of this
+     * method to {@code public} if using a different port is allowed.
+     */
     @SuppressWarnings("unchecked")
     protected SELF httpHost(String httpHost) {
       this.httpHost = trimScheme(httpHost);
       return (SELF) this;
     }
 
+    /**
+     * Set port for gRPC requests. Subclasses may increase the visibility of this
+     * method to {@code public} if using a different port is allowed.
+     */
     @SuppressWarnings("unchecked")
     protected SELF grpcHost(String grpcHost) {
       this.grpcHost = trimScheme(grpcHost);
@@ -80,24 +92,41 @@ public record Config(
       return url.replaceFirst("^https?\\/\\/", "");
     }
 
+    /**
+     * Provide a {@link TrustManagerFactory}. Subclasses which support
+     * secure connection should expose this method.
+     */
     @SuppressWarnings("unchecked")
     protected SELF trustManagerFactory(TrustManagerFactory tmf) {
       this.trustManagerFactory = tmf;
       return (SELF) this;
     }
 
+    /**
+     * Set a single request header. The client does not support header lists,
+     * so there is no equivalent {@code addHeader} to append to existing header.
+     * This will be applied both to REST and gRPC requests.
+     */
     @SuppressWarnings("unchecked")
     public SELF setHeader(String key, String value) {
       this.headers.put(key, value);
       return (SELF) this;
     }
 
+    /**
+     * Set multiple request headers.
+     * This will be applied both to REST and gRPC requests.
+     */
     @SuppressWarnings("unchecked")
     public SELF setHeaders(Map<String, String> headers) {
-      this.headers = Map.copyOf(headers);
+      this.headers.putAll(Map.copyOf(headers));
       return (SELF) this;
     }
 
+    /**
+     * Weaviate will use the URL in this header to call Weaviate Embeddings
+     * Service if an appropriate vectorizer is configured for collection.
+     */
     private static final String HEADER_X_WEAVIATE_CLUSTER_URL = "X-Weaviate-Cluster-URL";
 
     /**
@@ -113,6 +142,8 @@ public record Config(
 
     @Override
     public Config build() {
+      // For clusters hosted on Weaviate Cloud, Weaviate Embedding Service
+      // will be available under the same domain.
       if (isWeaviateDomain(httpHost) && tokenProvider != null) {
         setHeader(HEADER_X_WEAVIATE_CLUSTER_URL, "https://" + httpHost + ":" + httpPort);
       }
@@ -120,6 +151,18 @@ public record Config(
     }
   }
 
+  /**
+   * Configuration for Weaviate instances deployed locally.
+   *
+   * <p>
+   * Has sane defaults that match standard Weaviate deployment configuration:
+   * <ul>
+   * <li>{@code scheme: http}</li>
+   * <li>{@code host: localhost}</li>
+   * <li>{@code httpPort: 8080}</li>
+   * <li>{@code grpcPort: 50051}</li>
+   * </ul>
+   */
   public static class Local extends Builder<Local> {
     public Local() {
       scheme("http");
@@ -128,23 +171,37 @@ public record Config(
       grpcPort(50051);
     }
 
+    /**
+     * Set a different hostname.
+     * This changes both {@code httpHost} and {@code grpcHost}.
+     */
     public Local host(String host) {
       httpHost(host);
       grpcHost(host);
       return this;
     }
 
+    /** Override default HTTP port. */
     public Local httpPort(int port) {
       this.httpPort = port;
       return this;
     }
 
+    /** Override default gRPC port. */
     public Local grpcPort(int port) {
       this.grpcPort = port;
       return this;
     }
   }
 
+  /**
+   * Configuration for instances hosted on Weaviate Cloud.
+   * {@link Config#WeaviateCloud} will create a secure client
+   * with {@code schema: https} and {@code http-/grpcPort: 443}.
+   *
+   * Custom SSL certificates are suppored via
+   * {@link #trustManagerFactory}.
+   */
   public static class WeaviateCloud extends Builder<WeaviateCloud> {
     public WeaviateCloud(String httpHost, TokenProvider tokenProvider) {
       this(URI.create(httpHost), tokenProvider);
@@ -155,22 +212,46 @@ public record Config(
       super.httpHost(clusterUri.getHost() != null
           ? clusterUri.getHost() // https://[example.com]/about
           : clusterUri.getPath().split("/")[0]); // [example.com]/about
-      this.httpPort = 443;
       super.grpcHost("grpc-" + this.httpHost);
+      this.httpPort = 443;
       this.grpcPort = 443;
       this.tokenProvider = tokenProvider;
     }
 
+    /**
+     * Configure a custom TrustStore to validate third-party SSL certificates.
+     *
+     * <p>
+     * Usage:
+     *
+     * <pre>{@code
+     * // Create a TrustManagerFactory to validate custom certificates.
+     * TrustManagerFactory tmf;
+     * try (var keys = new FileInputStream("/path/to/custom/truststore.p12")) {
+     *   KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+     *   trustStore.load(myKeys, "secret-password".toCharArra());
+     *
+     *   tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+     *   tmf.init(trustStore);
+     * }
+     *
+     * // Pass it to wcd -> wcd.trustManagerFactory(tmf)
+     * }</pre>
+     */
     public WeaviateCloud trustManagerFactory(TrustManagerFactory tmf) {
       return super.trustManagerFactory(tmf);
     }
   }
 
+  /** Configuration for custom Weaviate deployements. */
   public static class Custom extends Builder<Custom> {
     /**
      * Scheme controls which protocol will be used for the database connection.
      * REST and gRPC ports will be automatically inferred from it:
      * <strong>443</strong> for HTTPS connection and <strong>80</strong> for HTTP.
+     *
+     * These can be overriden with {@link #httpPort(int)} and
+     * {@link #grpcPort(int)}.
      */
     public Custom scheme(String scheme) {
       httpPort("https".equals(scheme) ? 443 : 80);
@@ -178,31 +259,59 @@ public record Config(
       return super.scheme(scheme);
     }
 
+    /** Set HTTP hostname. */
     public Custom httpHost(String httpHost) {
       super.httpHost(httpHost);
       return this;
     }
 
+    /** Set HTTP port. */
     public Custom httpPort(int port) {
       this.httpPort = port;
       return this;
     }
 
+    /** Set gRPC hostname. */
     public Custom grpcHost(String grpcHost) {
       super.grpcHost(grpcHost);
       return this;
     }
 
+    /** Set gRPC port. */
     public Custom grpcPort(int port) {
       this.grpcPort = port;
       return this;
     }
 
+    /**
+     * Set authorization method. Setting this to {@code null} or omitting
+     * will not use any authorization mechanism.
+     */
     public Custom authorization(TokenProvider tokenProvider) {
       this.tokenProvider = tokenProvider;
       return this;
     }
 
+    /**
+     * Configure a custom TrustStore to validate third-party SSL certificates.
+     *
+     * <p>
+     * Usage:
+     *
+     * <pre>{@code
+     * // Create a TrustManagerFactory to validate custom certificates.
+     * TrustManagerFactory tmf;
+     * try (var keys = new FileInputStream("/path/to/custom/truststore.p12")) {
+     *   KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+     *   trustStore.load(myKeys, "secret-password".toCharArra());
+     *
+     *   tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+     *   tmf.init(trustStore);
+     * }
+     *
+     * // Pass it to custom -> custom.trustManagerFactory(tmf)
+     * }</pre>
+     */
     public Custom trustManagerFactory(TrustManagerFactory tmf) {
       return super.trustManagerFactory(tmf);
     }
