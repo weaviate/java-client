47aa39bf82578b7a25cc1aed70bb575206b729d6
dyma solovei <dyma@weaviate.io>
2025-08-07 01:25:26 +0200

    fix: add synchronization on ReuseTokenProvider
    Since HTTP client can use a thread pool executor,
    token provider is likely to be accessed concurrently.
    We only allow 1 thread at a time working with the token

--- src/main/java/io/weaviate/client6/v1/internal/ReuseTokenProvider.java
+++ src/main/java/io/weaviate/client6/v1/internal/ReuseTokenProvider.java
@@ -1,16 +1,18 @@
 package io.weaviate.client6.v1.internal;
 
+import javax.annotation.concurrent.ThreadSafe;
+
 /**
  * ReuseTokenProvider returns the same token as long as its valid and obtains a
  * new token from a {@link TokenProvider} otherwise.
  */
+@ThreadSafe
 final class ReuseTokenProvider implements TokenProvider {
   private TokenProvider provider;
   private Token token;
 
-  // TODO: this will need synchronization
   @Override
-  public Token getToken() {
+  public synchronized Token getToken() {
     if (token.isValid()) {
       return token;
     }
