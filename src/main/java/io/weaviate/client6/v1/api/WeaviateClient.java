ad6ea8b5a9f804ffce05856bb084fb716f9aee52
dyma solovei <dyma@weaviate.io>
2025-08-15 01:09:39 +0200

    feat: implement readiness check

--- src/main/java/io/weaviate/client6/v1/api/WeaviateClient.java
+++ src/main/java/io/weaviate/client6/v1/api/WeaviateClient.java
@@ -126,6 +126,11 @@ public class WeaviateClient implements Closeable {
     return this.restTransport.performRequest(null, IsLiveRequest._ENDPOINT);
   }
 
+  /** Ping the server for a readiness check. */
+  public boolean isReady() throws IOException {
+    return this.restTransport.performRequest(null, IsReadyRequest._ENDPOINT);
+  }
+
   /**
    * Close {@link #restTransport} and {@link #grpcTransport}
    * and release associated resources.
