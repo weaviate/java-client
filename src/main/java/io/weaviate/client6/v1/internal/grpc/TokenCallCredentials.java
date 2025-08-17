0fa33e6c7f35da857cf5a4e85a4d028f9153c0b6
dyma solovei <dyma@weaviate.io>
2025-08-17 21:47:46 +0200

    feat: use non-blocking token provider in the gRPC stub
    Add another interface method to AsyncTokenProvider to
    schedule the future on a custom executor.

--- src/main/java/io/weaviate/client6/v1/internal/grpc/TokenCallCredentials.java
+++ src/main/java/io/weaviate/client6/v1/internal/grpc/TokenCallCredentials.java
@@ -5,29 +5,40 @@ import java.util.concurrent.Executor;
 import io.grpc.CallCredentials;
 import io.grpc.Metadata;
 import io.grpc.Status;
+import io.weaviate.client6.v1.internal.AsyncTokenProvider;
 import io.weaviate.client6.v1.internal.TokenProvider;
 
-class TokenCallCredentials extends CallCredentials {
+class TokenCallCredentials extends CallCredentials implements AutoCloseable {
   private static final Metadata.Key<String> AUTHORIZATION = Metadata.Key.of("Authorization",
       Metadata.ASCII_STRING_MARSHALLER);
 
-  private final TokenProvider tokenProvider;
+  /**
+   * Since {@link #applyRequestMetadata} accepts an {@link Executor} anyways,
+   * we can always just use an async provider, instead of creating 2 separate
+   * instances for it.
+   */
+  private final AsyncTokenProvider tokenProviderAsync;
 
   TokenCallCredentials(TokenProvider tokenProvider) {
-    this.tokenProvider = tokenProvider;
+    this.tokenProviderAsync = AsyncTokenProvider.wrap(tokenProvider);
   }
 
   @Override
   public void applyRequestMetadata(RequestInfo requestInfo, Executor executor, MetadataApplier metadataApplier) {
-    executor.execute(() -> {
-      try {
-        var headers = new Metadata();
-        var token = tokenProvider.getToken().accessToken();
-        headers.put(AUTHORIZATION, "Bearer " + token);
-        metadataApplier.apply(headers);
-      } catch (Exception e) {
-        metadataApplier.fail(Status.UNAUTHENTICATED.withCause(e));
-      }
-    });
+    tokenProviderAsync.getToken(executor)
+        .whenComplete((tok, ex) -> {
+          if (ex != null) {
+            metadataApplier.fail(Status.UNAUTHENTICATED.withCause(ex));
+            return;
+          }
+          var headers = new Metadata();
+          headers.put(AUTHORIZATION, "Bearer " + tok.accessToken());
+          metadataApplier.apply(headers);
+        });
+  }
+
+  @Override
+  public void close() throws Exception {
+    tokenProviderAsync.close();
   }
 }
