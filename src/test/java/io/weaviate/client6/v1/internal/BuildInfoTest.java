10ae85d13dd8843db173438ae07c17749c21c321
dyma solovei <dyma@weaviate.io>
2025-08-22 13:27:16 +0200

    test: skip test if no git branch or git commit not found locally
    Use CI-friendly git path (/usr/bin/git) just in case it's not
    in the GH runner's PATH.

--- src/test/java/io/weaviate/client6/v1/internal/BuildInfoTest.java
+++ src/test/java/io/weaviate/client6/v1/internal/BuildInfoTest.java
@@ -5,31 +5,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Assume;
 import org.junit.Test;
 
 public class BuildInfoTest {
+  private static final String BRANCH = gitBranch();
+  private static final String COMMIT_ID = gitCommit();
 
   @Test
   public void testBuildInfo() throws IOException {
-    Assertions.assertThat(BuildInfo.BRANCH).as("branch").isEqualTo(gitBranch());
-    Assertions.assertThat(BuildInfo.COMMIT_ID).as("commit.full").isEqualTo(gitCommit());
-    Assertions.assertThat(gitCommit()).as("commit.abbrev").startsWith(BuildInfo.COMMIT_ID_ABBREV);
+    Assume.assumeNotNull(BRANCH, COMMIT_ID);
+    Assume.assumeTrue("found git branch", !BRANCH.isBlank());
+    Assume.assumeTrue("found git commit", !COMMIT_ID.isBlank());
+
+    Assertions.assertThat(BuildInfo.BRANCH).as("branch").isEqualTo(BRANCH);
+    Assertions.assertThat(BuildInfo.COMMIT_ID).as("commit.full").isEqualTo(COMMIT_ID);
+    Assertions.assertThat(COMMIT_ID).as("commit.abbrev").startsWith(BuildInfo.COMMIT_ID_ABBREV);
   }
 
   /** Get current non-abbreviated Git commit hash. */
-  private static String gitCommit() throws IOException {
-    return runCommand("git", "rev-parse", "HEAD");
+  private static String gitCommit() {
+    return runCommand("/usr/bin/git", "rev-parse", "HEAD");
   }
 
   /** Get current git branch. */
-  private static String gitBranch() throws IOException {
-    return runCommand("git", "branch", "--show-current");
+  private static String gitBranch() {
+    return runCommand("/usr/bin/git", "branch", "--show-current");
   }
 
   /** Run shell command and return the output as multi-line string. */
-  private static String runCommand(String... cmdarray) throws IOException {
-    var process = Runtime.getRuntime().exec(cmdarray);
-    var r = new BufferedReader(new InputStreamReader(process.getInputStream()));
-    return String.join("\n", (Iterable<String>) () -> r.lines().iterator());
+  private static String runCommand(String... cmdarray) {
+    try {
+      var process = Runtime.getRuntime().exec(cmdarray);
+      var r = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      return String.join("\n", (Iterable<String>) () -> r.lines().iterator());
+    } catch (IOException e) {
+      return null;
+    }
   }
 }
