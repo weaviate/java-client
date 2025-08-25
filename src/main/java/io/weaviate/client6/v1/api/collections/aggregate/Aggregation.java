db4d230a67575c2a9120968a4c84809d54cd957b
dyma solovei <dyma@weaviate.io>
2025-08-25 18:34:08 +0200

    refactor: put aggregation-factories behind Aggregate

--- src/main/java/io/weaviate/client6/v1/api/collections/aggregate/Aggregation.java
+++ src/main/java/io/weaviate/client6/v1/api/collections/aggregate/Aggregation.java
@@ -22,31 +22,6 @@ public record Aggregation(
     return fn.apply(new Builder(objectFilter)).build();
   }
 
-  public static final PropertyAggregation text(String property,
-      Function<TextAggregation.Builder, ObjectBuilder<TextAggregation>> fn) {
-    return TextAggregation.of(property, fn);
-  }
-
-  public static final PropertyAggregation integer(String property,
-      Function<IntegerAggregation.Builder, ObjectBuilder<IntegerAggregation>> fn) {
-    return IntegerAggregation.of(property, fn);
-  }
-
-  public static final PropertyAggregation bool(String property,
-      Function<BooleanAggregation.Builder, ObjectBuilder<BooleanAggregation>> fn) {
-    return BooleanAggregation.of(property, fn);
-  }
-
-  public static final PropertyAggregation date(String property,
-      Function<DateAggregation.Builder, ObjectBuilder<DateAggregation>> fn) {
-    return DateAggregation.of(property, fn);
-  }
-
-  public static final PropertyAggregation number(String property,
-      Function<NumberAggregation.Builder, ObjectBuilder<NumberAggregation>> fn) {
-    return NumberAggregation.of(property, fn);
-  }
-
   public Aggregation(Builder builder) {
     this(
         builder.objectFilter,
