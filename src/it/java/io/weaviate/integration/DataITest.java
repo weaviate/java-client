0470e179521b403c1d2b876a72b28f5533e17c91
dyma solovei <dyma@weaviate.io>
2025-08-25 15:05:12 +0200

    test: add test case for reading/writing all property types

--- src/it/java/io/weaviate/integration/DataITest.java
+++ src/it/java/io/weaviate/integration/DataITest.java
@@ -1,7 +1,9 @@
 package io.weaviate.integration;
 
 import java.io.IOException;
+import java.time.OffsetDateTime;
 import java.util.Map;
+import java.util.UUID;
 
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.InstanceOfAssertFactories;
@@ -406,5 +408,50 @@ public class DataITest extends ConcurrentTest {
 
     // Act
     things.data.insert(Map.of(), thing -> thing.uuid(thing_1.uuid()));
+  }
+
+  @Test
+  public void testDataTypes() throws IOException {
+    // Arrange
+    var nsDataTypes = ns("DataTypes");
+
+    // BLOB type is omitted because a base64-encoded image
+    // isn't doing the failure message any favours.
+    // It's tested in other test cases above.
+    client.collections.create(
+        nsDataTypes, c -> c
+            .properties(
+                Property.text("prop_text"),
+                Property.integer("prop_integer"),
+                Property.number("prop_number"),
+                Property.bool("prop_bool"),
+                Property.date("prop_date"),
+                Property.uuid("prop_uuid")));
+
+    var types = client.collections.use(nsDataTypes);
+
+    var now = OffsetDateTime.now();
+    var uuid = UUID.randomUUID();
+
+    Map<String, Object> want = Map.of(
+        "prop_text", "Hello, World!",
+        "prop_integer", 1L,
+        "prop_number", 1D,
+        "prop_bool", true,
+        "prop_date", now,
+        "prop_uuid", uuid);
+    var returnProperties = want.keySet().toArray(String[]::new);
+
+    // Act
+    var object = types.data.insert(want);
+    var got = types.query.byId(object.uuid(),
+        q -> q.returnProperties(returnProperties));
+
+    // Assert
+    Assertions.assertThat(got).get()
+        .extracting(WeaviateObject::properties)
+        .asInstanceOf(InstanceOfAssertFactories.map(String.class, Object.class))
+        .containsAllEntriesOf(want);
+
   }
 }
--- src/main/java/io/weaviate/client6/v1/api/collections/DataType.java
+++ src/main/java/io/weaviate/client6/v1/api/collections/DataType.java
@@ -7,11 +7,11 @@ import com.google.common.collect.ImmutableSet;
 public interface DataType {
   public static final String TEXT = "text";
   public static final String INT = "int";
-  public static final String BLOB = "blob";
+  public static final String NUMBER = "number";
   public static final String BOOL = "boolean";
+  public static final String BLOB = "blob";
   public static final String DATE = "date";
   public static final String UUID = "uuid";
-  public static final String NUMBER = "number";
 
   public static final Set<String> KNOWN_TYPES = ImmutableSet.of(TEXT, INT, BLOB, BOOL, DATE, UUID, NUMBER);
 }
