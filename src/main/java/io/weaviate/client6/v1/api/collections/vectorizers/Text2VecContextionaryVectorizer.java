e0a827b4c40dc01bbbd76a97df73e3d9a690aa5d
dyma solovei <dyma@weaviate.io>
2025-08-27 12:18:40 +0200

    feat: add sourceProperties to text-based vectorizers

--- src/main/java/io/weaviate/client6/v1/api/collections/vectorizers/Text2VecContextionaryVectorizer.java
+++ src/main/java/io/weaviate/client6/v1/api/collections/vectorizers/Text2VecContextionaryVectorizer.java
@@ -1,5 +1,9 @@
 package io.weaviate.client6.v1.api.collections.vectorizers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 
 import com.google.gson.annotations.SerializedName;
@@ -11,13 +15,16 @@ import io.weaviate.client6.v1.internal.ObjectBuilder;
 public record Text2VecContextionaryVectorizer(
     /**
      * Weaviate defaults to {@code true} if the value is not provided.
-     * Because text2vec-contextionary cannot handle understores in collection names,
+     * Because text2vec-contextionary cannot handle underscores in collection names,
      * this quickly becomes inconvenient.
      *
      * To avoid that we send "vectorizeClassName": false all the time
      * and make it impossible to enable this feature, as it is deprecated.
      */
     @Deprecated @SerializedName("vectorizeClassName") boolean vectorizeCollectionName,
+    /** Properties included in the embedding. */
+    @SerializedName("sourceProperties") List<String> sourceProperties,
+    /** Vector index configuration. */
     VectorIndex vectorIndex) implements Vectorizer {
 
   @Override
@@ -42,20 +49,41 @@ public record Text2VecContextionaryVectorizer(
   /**
    * Canonical constructor always sets {@link #vectorizeCollectionName} to false.
    */
-  public Text2VecContextionaryVectorizer(boolean vectorizeCollectionName, VectorIndex vectorIndex) {
+  public Text2VecContextionaryVectorizer(boolean vectorizeCollectionName, List<String> sourceProperties,
+      VectorIndex vectorIndex) {
     this.vectorizeCollectionName = false;
     this.vectorIndex = vectorIndex;
+    this.sourceProperties = Collections.emptyList();
   }
 
   public Text2VecContextionaryVectorizer(Builder builder) {
-    this(builder.vectorizeCollectionName, builder.vectorIndex);
+    this(builder.vectorizeCollectionName, builder.sourceProperties, builder.vectorIndex);
   }
 
   public static class Builder implements ObjectBuilder<Text2VecContextionaryVectorizer> {
     private final boolean vectorizeCollectionName = false;
 
+    private List<String> sourceProperties = new ArrayList<>();
     private VectorIndex vectorIndex = VectorIndex.DEFAULT_VECTOR_INDEX;
 
+    /** Add properties to include in the embedding. */
+    public Builder sourceProperties(String... properties) {
+      return sourceProperties(Arrays.asList(properties));
+    }
+
+    /** Add properties to include in the embedding. */
+    public Builder sourceProperties(List<String> properties) {
+      this.sourceProperties.addAll(properties);
+      return this;
+    }
+
+    /**
+     * Override default vector index configuration.
+     *
+     * <a href=
+     * "https://docs.weaviate.io/weaviate/config-refs/indexing/vector-index#hnsw-index-parameters">HNSW</a>
+     * is the default vector index.
+     */
     public Builder vectorIndex(VectorIndex vectorIndex) {
       this.vectorIndex = vectorIndex;
       return this;
