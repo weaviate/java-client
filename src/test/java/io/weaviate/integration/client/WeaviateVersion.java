a4276a9b76d3b7634d3f7cc4d180ffee29728ad3
dyma solovei <dyma@weaviate.io>
2025-09-02 15:43:33 +0200

    test: fix typo

--- src/test/java/io/weaviate/integration/client/WeaviateVersion.java
+++ src/test/java/io/weaviate/integration/client/WeaviateVersion.java
@@ -6,7 +6,7 @@ public class WeaviateVersion {
   public static final String WEAVIATE_IMAGE = "1.33.0-rc.0";
 
   // to be set according to weaviate docker image
-  public static final String EXPECTED_WEAVIATE_VERSION = "1.32.0-rc.0";
+  public static final String EXPECTED_WEAVIATE_VERSION = "1.33.0-rc.0";
   // to be set according to weaviate docker image
   public static final String EXPECTED_WEAVIATE_GIT_HASH = "7a6f802";
 
