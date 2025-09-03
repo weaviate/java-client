d8e8ba4019bce54b7c6890be3490374c389deaf8
dyma solovei <dyma@weaviate.io>
2025-09-03 16:53:05 +0200

    test: delete server-related assertion
    This test simply checks that server returns some string exactly,
    which is based _entirely_ on how the server is configured and
    has no relation to the client-side logic. It is deserialized in
    and Object, so there is no special behavior to verify.
    
    The assertion does fail with v1.33.0-rc.0 though, because
    the server started returning a different string.
    
    Once again, this string is not something that affects client
    behavior in any way.

--- src/test/java/io/weaviate/integration/tests/misc/MiscTestSuite.java
+++ src/test/java/io/weaviate/integration/tests/misc/MiscTestSuite.java
@@ -1,12 +1,13 @@
 package io.weaviate.integration.tests.misc;
 
-import io.weaviate.client.base.Result;
-import io.weaviate.client.v1.misc.model.Meta;
 import static io.weaviate.integration.client.WeaviateVersion.EXPECTED_WEAVIATE_VERSION;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import io.weaviate.client.base.Result;
+import io.weaviate.client.v1.misc.model.Meta;
 
 public class MiscTestSuite {
   public static void assertLivenessOrReadiness(Result<Boolean> result) {
@@ -19,8 +20,5 @@ public class MiscTestSuite {
     assertNull(meta.getError());
     assertEquals("http://[::]:8080", meta.getResult().getHostname());
     assertEquals(EXPECTED_WEAVIATE_VERSION, meta.getResult().getVersion());
-    assertEquals("{backup-filesystem={backupsPath=/tmp/backups}, " +
-      "generative-openai={documentationHref=https://platform.openai.com/docs/api-reference/completions, name=Generative Search - OpenAI}, " +
-      "text2vec-contextionary={version=en0.16.0-v1.2.1, wordCount=818072.0}}", meta.getResult().getModules().toString());
   }
 }
