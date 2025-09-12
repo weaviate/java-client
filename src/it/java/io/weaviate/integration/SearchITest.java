162f860dc20b440f9e23013fa9b5ce62f8c3318a
dyma solovei <dyma@weaviate.io>
2025-09-12 16:35:20 +0200

    feat: request all metadata with Metadata.ALL

--- src/it/java/io/weaviate/integration/SearchITest.java
+++ src/it/java/io/weaviate/integration/SearchITest.java
@@ -415,4 +415,49 @@ public class SearchITest extends ConcurrentTest {
       }
     }
   }
+
+  @Test
+  public void testMetadataAll() throws IOException {
+    // Arrange
+    var nsThings = ns("Things");
+    client.collections.create(nsThings,
+        c -> c
+            .properties(Property.text("name"))
+            .vectors(Vectorizers.text2vecContextionary(
+                t2v -> t2v.sourceProperties("name"))));
+
+    var things = client.collections.use(nsThings);
+    var frisbee = things.data.insert(Map.of("name", "orange disc"));
+
+    // Act
+    var gotHybrid = things.query.hybrid("orange", q -> q
+        .queryProperties("name")
+        .returnMetadata(Metadata.ALL));
+
+    var gotNearText = things.query.nearText("frisbee", q -> q
+        .returnMetadata(Metadata.ALL));
+
+    // Assert
+    var metadataHybrid = Assertions.assertThat(gotHybrid.objects())
+        .hasSize(1)
+        .extracting(WeaviateObject::metadata)
+        .first().actual();
+
+    Assertions.assertThat(metadataHybrid.uuid()).as("uuid").isNotNull().isEqualTo(frisbee.uuid());
+    Assertions.assertThat(metadataHybrid.creationTimeUnix()).as("creationTimeUnix").isNotNull();
+    Assertions.assertThat(metadataHybrid.lastUpdateTimeUnix()).as("lastUpdateTimeUnix").isNotNull();
+    Assertions.assertThat(metadataHybrid.score()).as("score").isNotNull();
+    Assertions.assertThat(metadataHybrid.explainScore()).as("explainScore").isNotNull().isNotEqualTo("");
+
+    var metadataNearText = Assertions.assertThat(gotNearText.objects())
+        .hasSize(1)
+        .extracting(WeaviateObject::metadata)
+        .first().actual();
+
+    Assertions.assertThat(metadataNearText.uuid()).as("uuid").isNotNull().isEqualTo(frisbee.uuid());
+    Assertions.assertThat(metadataNearText.creationTimeUnix()).as("creationTimeUnix").isNotNull();
+    Assertions.assertThat(metadataNearText.lastUpdateTimeUnix()).as("lastUpdateTimeUnix").isNotNull();
+    Assertions.assertThat(metadataNearText.distance()).as("distance").isNotNull();
+    Assertions.assertThat(metadataNearText.certainty()).as("certainty").isNotNull();
+  }
 }
