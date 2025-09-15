70b63000b5e398d1bed6cd2bdec6b85509db1b46
dyma solovei <dyma@weaviate.io>
2025-09-15 13:23:10 +0200

    chore: improve Vectors::toString representation

---
+++ src/test/java/io/weaviate/client6/v1/api/collections/VectorsTest.java
@@ -0,0 +1,20 @@
+package io.weaviate.client6.v1.api.collections;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class VectorsTest {
+  @Test
+  public void testToString_1d() {
+    var vector = Vectors.of(new float[] { 1, 2, 3 });
+    var got = vector.toString();
+    Assertions.assertThat(got).isEqualTo("Vectors(default=[1.0, 2.0, 3.0])");
+  }
+
+  @Test
+  public void testToString_2d() {
+    var vector = Vectors.of(new float[][] { { 1, 2, 3 }, { 1, 2, 3 } });
+    var got = vector.toString();
+    Assertions.assertThat(got).isEqualTo("Vectors(default=[[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]])");
+  }
+}
