da953d6582985abaa85d4ded4af7feab2f57d84a
dyma solovei <dyma@weaviate.io>
2025-09-16 13:20:26 +0200

    test: add another test case for Vectors::toString

--- src/test/java/io/weaviate/client6/v1/api/collections/VectorsTest.java
+++ src/test/java/io/weaviate/client6/v1/api/collections/VectorsTest.java
@@ -17,4 +17,13 @@ public class VectorsTest {
     var got = vector.toString();
     Assertions.assertThat(got).isEqualTo("Vectors(default=[[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]])");
   }
+
+  @Test
+  public void testToString_multiple() {
+    var title = Vectors.of("title", new float[] { 1, 2, 3 });
+    var body = Vectors.of("body", new float[][] { { 1, 2, 3 }, { 1, 2, 3 } });
+    var vectors = new Vectors(title, body);
+    var got = vectors.toString();
+    Assertions.assertThat(got).isEqualTo("Vectors(title=[1.0, 2.0, 3.0], body=[[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]])");
+  }
 }
