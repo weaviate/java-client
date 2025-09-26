67c873d722521610e5507cbb1d0086629b7d9fff
dyma solovei <dyma@weaviate.io>
2025-09-26 15:57:17 +0200

    chore: prevent NPEs on mapy copy / Stream.collect

--- src/main/java/io/weaviate/client6/v1/internal/orm/MapReader.java
+++ src/main/java/io/weaviate/client6/v1/internal/orm/MapReader.java
@@ -1,16 +1,19 @@
 package io.weaviate.client6.v1.internal.orm;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapReader implements PropertiesReader<Map<String, Object>> {
   private final Map<String, Object> properties;
 
   public MapReader(Map<String, Object> properties) {
-    this.properties = properties;
+    // Defensive copy to ensure original properties are not modified
+    this.properties = Collections.unmodifiableMap(new HashMap<>(properties));
   }
 
   @Override
   public Map<String, Object> readProperties() {
-    return Map.copyOf(properties); // ensure original properties immutable
+    return properties;
   }
 }
