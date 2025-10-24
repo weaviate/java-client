d15dcb2d6f41b87dc188f9891333d7b46309be80
dyma solovei <dyma@weaviate.io>
2025-10-24 14:58:14 +0200

    feat: add text2vec-cohere vectorizer

---
+++ src/main/java/io/weaviate/client6/v1/api/collections/vectorizers/Text2VecCohereVectorizer.java
@@ -0,0 +1,159 @@
+package io.weaviate.client6.v1.api.collections.vectorizers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
+import com.google.gson.annotations.SerializedName;
+
+import io.weaviate.client6.v1.api.collections.Quantization;
+import io.weaviate.client6.v1.api.collections.VectorConfig;
+import io.weaviate.client6.v1.api.collections.VectorIndex;
+import io.weaviate.client6.v1.internal.ObjectBuilder;
+
+public record Text2VecCohereVectorizer(
+    @SerializedName("model") String model,
+    @SerializedName("truncate") Truncate truncate,
+    @SerializedName("baseUrl") String baseUrl,
+
+    /**
+     * Weaviate defaults to {@code true} if the value is not provided.
+     * Because text2vec-cohere cannot handle underscores in collection names,
+     * this quickly becomes inconvenient.
+     *
+     * To avoid that we send "vectorizeClassName": false all the time
+     * and make it impossible to enable this feature, as it is deprecated.
+     */
+    @Deprecated @SerializedName("vectorizeClassName") boolean vectorizeCollectionName,
+    /** Properties included in the embedding. */
+    @SerializedName("sourceProperties") List<String> sourceProperties,
+    /** Vector index configuration. */
+    VectorIndex vectorIndex,
+    /** Vector quantization method. */
+    Quantization quantization) implements VectorConfig {
+
+  @Override
+  public VectorConfig.Kind _kind() {
+    return VectorConfig.Kind.TEXT2VEC_COHERE;
+  }
+
+  @Override
+  public Object _self() {
+    return this;
+  }
+
+  public enum Truncate {
+    @SerializedName("NONE")
+    NONE,
+    @SerializedName("START")
+    START,
+    @SerializedName("END")
+    END,
+    @SerializedName("LEFT")
+    LEFT,
+    @SerializedName("RIGHT")
+    RIGHT;
+  }
+
+  public static Text2VecCohereVectorizer of() {
+    return of(ObjectBuilder.identity());
+  }
+
+  public static Text2VecCohereVectorizer of(
+      Function<Builder, ObjectBuilder<Text2VecCohereVectorizer>> fn) {
+    return fn.apply(new Builder()).build();
+  }
+
+  /**
+   * Canonical constructor always sets {@link #vectorizeCollectionName} to false.
+   */
+  public Text2VecCohereVectorizer(
+      String model,
+      Truncate truncate,
+      String baseUrl,
+
+      boolean vectorizeCollectionName,
+      List<String> sourceProperties,
+      VectorIndex vectorIndex,
+      Quantization quantization) {
+    this.model = model;
+    this.truncate = truncate;
+    this.baseUrl = baseUrl;
+
+    this.vectorizeCollectionName = false;
+    this.sourceProperties = Collections.emptyList();
+    this.vectorIndex = vectorIndex;
+    this.quantization = quantization;
+  }
+
+  public Text2VecCohereVectorizer(Builder builder) {
+    this(
+        builder.model,
+        builder.truncate,
+        builder.baseUrl,
+        builder.vectorizeCollectionName,
+        builder.sourceProperties,
+        builder.vectorIndex,
+        builder.quantization);
+  }
+
+  public static class Builder implements ObjectBuilder<Text2VecCohereVectorizer> {
+    private final boolean vectorizeCollectionName = false;
+    private Quantization quantization;
+    private List<String> sourceProperties = new ArrayList<>();
+    private VectorIndex vectorIndex = VectorIndex.DEFAULT_VECTOR_INDEX;
+
+    private String model;
+    private Truncate truncate;
+    private String baseUrl;
+
+    public Builder baseUrl(String baseUrl) {
+      this.baseUrl = baseUrl;
+      return this;
+    }
+
+    public Builder model(String model) {
+      this.model = model;
+      return this;
+    }
+
+    public Builder truncate(Truncate truncate) {
+      this.truncate = truncate;
+      return this;
+    }
+
+    /** Add properties to include in the embedding. */
+    public Builder sourceProperties(String... properties) {
+      return sourceProperties(Arrays.asList(properties));
+    }
+
+    /** Add properties to include in the embedding. */
+    public Builder sourceProperties(List<String> properties) {
+      this.sourceProperties.addAll(properties);
+      return this;
+    }
+
+    /**
+     * Override default vector index configuration.
+     *
+     * <a href=
+     * "https://docs.weaviate.io/weaviate/config-refs/indexing/vector-index#hnsw-index-parameters">HNSW</a>
+     * is the default vector index.
+     */
+    public Builder vectorIndex(VectorIndex vectorIndex) {
+      this.vectorIndex = vectorIndex;
+      return this;
+    }
+
+    public Builder quantization(Quantization quantization) {
+      this.quantization = quantization;
+      return this;
+    }
+
+    public Text2VecCohereVectorizer build() {
+      return new Text2VecCohereVectorizer(this);
+    }
+  }
+}
