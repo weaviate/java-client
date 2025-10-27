3dd1de812d8e6828944ea3aa7c2ff49a5c4aa2b9
dyma solovei <dyma@weaviate.io>
2025-10-27 14:42:52 +0100

    feat: add text2vec-huggingface module

--- src/main/java/io/weaviate/client6/v1/api/collections/vectorizers/Text2VecHuggingfaceVectorizer.java
+++ src/main/java/io/weaviate/client6/v1/api/collections/vectorizers/Text2VecHuggingfaceVectorizer.java
@@ -13,10 +13,14 @@ import io.weaviate.client6.v1.api.collections.VectorConfig;
 import io.weaviate.client6.v1.api.collections.VectorIndex;
 import io.weaviate.client6.v1.internal.ObjectBuilder;
 
-public record Text2VecCohereVectorizer(
+public record Text2VecHuggingfaceVectorizer(
+    @SerializedName("endpointURL") String baseUrl,
     @SerializedName("model") String model,
-    @SerializedName("truncate") Truncate truncate,
-    @SerializedName("baseUrl") String baseUrl,
+    @SerializedName("passageModel") String passageModel,
+    @SerializedName("queryModel") String queryModel,
+    @SerializedName("useCache") Boolean useCache,
+    @SerializedName("useGPU") Boolean useGpu,
+    @SerializedName("waitForModel") Boolean waitForModel,
 
     /**
      * Weaviate defaults to {@code true} if the value is not provided.
@@ -33,7 +37,7 @@ public record Text2VecCohereVectorizer(
 
   @Override
   public VectorConfig.Kind _kind() {
-    return VectorConfig.Kind.TEXT2VEC_COHERE;
+    return VectorConfig.Kind.TEXT2VEC_HUGGINGFACE;
   }
 
   @Override
@@ -41,43 +45,38 @@ public record Text2VecCohereVectorizer(
     return this;
   }
 
-  public enum Truncate {
-    @SerializedName("NONE")
-    NONE,
-    @SerializedName("START")
-    START,
-    @SerializedName("END")
-    END,
-    @SerializedName("LEFT")
-    LEFT,
-    @SerializedName("RIGHT")
-    RIGHT;
-  }
-
-  public static Text2VecCohereVectorizer of() {
+  public static Text2VecHuggingfaceVectorizer of() {
     return of(ObjectBuilder.identity());
   }
 
-  public static Text2VecCohereVectorizer of(
-      Function<Builder, ObjectBuilder<Text2VecCohereVectorizer>> fn) {
+  public static Text2VecHuggingfaceVectorizer of(
+      Function<Builder, ObjectBuilder<Text2VecHuggingfaceVectorizer>> fn) {
     return fn.apply(new Builder()).build();
   }
 
   /**
    * Canonical constructor always sets {@link #vectorizeCollectionName} to false.
    */
-  public Text2VecCohereVectorizer(
-      String model,
-      Truncate truncate,
+  public Text2VecHuggingfaceVectorizer(
       String baseUrl,
+      String model,
+      String passageModel,
+      String queryModel,
+      Boolean useCache,
+      Boolean useGpu,
+      Boolean waitForModel,
 
       boolean vectorizeCollectionName,
       List<String> sourceProperties,
       VectorIndex vectorIndex,
       Quantization quantization) {
-    this.model = model;
-    this.truncate = truncate;
     this.baseUrl = baseUrl;
+    this.model = model;
+    this.passageModel = passageModel;
+    this.queryModel = queryModel;
+    this.useCache = useCache;
+    this.useGpu = useGpu;
+    this.waitForModel = waitForModel;
 
     this.vectorizeCollectionName = false;
     this.sourceProperties = Collections.emptyList();
@@ -85,26 +84,34 @@ public record Text2VecCohereVectorizer(
     this.quantization = quantization;
   }
 
-  public Text2VecCohereVectorizer(Builder builder) {
+  public Text2VecHuggingfaceVectorizer(Builder builder) {
     this(
-        builder.model,
-        builder.truncate,
         builder.baseUrl,
+        builder.model,
+        builder.passageModel,
+        builder.queryModel,
+        builder.useCache,
+        builder.useGpu,
+        builder.waitForModel,
         builder.vectorizeCollectionName,
         builder.sourceProperties,
         builder.vectorIndex,
         builder.quantization);
   }
 
-  public static class Builder implements ObjectBuilder<Text2VecCohereVectorizer> {
+  public static class Builder implements ObjectBuilder<Text2VecHuggingfaceVectorizer> {
     private final boolean vectorizeCollectionName = false;
     private Quantization quantization;
     private List<String> sourceProperties = new ArrayList<>();
     private VectorIndex vectorIndex = VectorIndex.DEFAULT_VECTOR_INDEX;
 
-    private String model;
-    private Truncate truncate;
     private String baseUrl;
+    private String model;
+    private String passageModel;
+    private String queryModel;
+    private Boolean useCache;
+    private Boolean useGpu;
+    private Boolean waitForModel;
 
     public Builder baseUrl(String baseUrl) {
       this.baseUrl = baseUrl;
@@ -116,8 +123,30 @@ public record Text2VecCohereVectorizer(
       return this;
     }
 
-    public Builder truncate(Truncate truncate) {
-      this.truncate = truncate;
+    /** The model to use for passage vectorization. */
+    public Builder passageModel(String passageModel) {
+      this.passageModel = passageModel;
+      return this;
+    }
+
+    /** The model to use for query vectorization. */
+    public Builder queryModel(String queryModel) {
+      this.queryModel = queryModel;
+      return this;
+    }
+
+    public Builder useCache(boolean useCache) {
+      this.useCache = useCache;
+      return this;
+    }
+
+    public Builder useGpu(boolean useGpu) {
+      this.useGpu = useGpu;
+      return this;
+    }
+
+    public Builder waitForModel(boolean waitForModel) {
+      this.waitForModel = waitForModel;
       return this;
     }
 
@@ -149,8 +178,8 @@ public record Text2VecCohereVectorizer(
       return this;
     }
 
-    public Text2VecCohereVectorizer build() {
-      return new Text2VecCohereVectorizer(this);
+    public Text2VecHuggingfaceVectorizer build() {
+      return new Text2VecHuggingfaceVectorizer(this);
     }
   }
 }
