1428b6a7d92f0bb27b24d503d8e2eefce5959efe
dyma solovei <dyma@weaviate.io>
2025-10-27 14:45:40 +0100

    feat: add text2vec-morph vectorizer

--- src/main/java/io/weaviate/client6/v1/api/collections/VectorConfig.java
+++ src/main/java/io/weaviate/client6/v1/api/collections/VectorConfig.java
@@ -31,6 +31,7 @@ public interface VectorConfig extends TaggedUnion<VectorConfig.Kind, Object> {
     TEXT2VEC_CONTEXTIONARY("text2vec-contextionary"),
     TEXT2VEC_COHERE("text2vec-cohere"),
     TEXT2VEC_HUGGINGFACE("text2vec-huggingface"),
+    TEXT2VEC_MORPH("text2vec-morph"),
     TEXT2VEC_WEAVIATE("text2vec-weaviate"),
     MULTI2VEC_CLIP("multi2vec-clip");
 
