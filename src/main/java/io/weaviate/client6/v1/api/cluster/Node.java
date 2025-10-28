b73e8b30e74611f1a15eeecbe65508b5b230d773
dyma solovei <dyma@weaviate.io>
2025-10-28 12:20:25 +0100

    refactor: introduce enum for node status

--- src/main/java/io/weaviate/client6/v1/api/cluster/Node.java
+++ src/main/java/io/weaviate/client6/v1/api/cluster/Node.java
@@ -6,9 +6,21 @@ import com.google.gson.annotations.SerializedName;
 
 public record Node(
     @SerializedName("name") String name,
-    @SerializedName("status") String status,
+    @SerializedName("status") NodeStatus status,
+    /** Commit hash of the Weaviate build the node is running. */
     @SerializedName("gitHash") String gitHash,
+    /** Weaviate version the node is running. */
     @SerializedName("version") String version,
+    /**
+     * Can be {@code null} if "minimal" output is requested.
+     *
+     * @see NodeVerbosity#MINIMAL.
+     */
     @SerializedName("stats") CollectionStats stats,
+    /**
+     * Can be {@code null} if "minimal" output is requested.
+     *
+     * @see NodeVerbosity#MINIMAL.
+     */
     @SerializedName("shards") List<Shard> shards) {
 }
