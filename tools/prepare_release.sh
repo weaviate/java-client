42edd3a64f7c2d9064e2daeb3f8b5eeea12506a4
dyma solovei <dyma@weaviate.io>
2025-11-21 16:52:22 +0100

    ci: update release script to recognize -rc version suffix

--- tools/prepare_release.sh
+++ tools/prepare_release.sh
@@ -23,8 +23,8 @@ if git rev-parse "$VERSION" >/dev/null 2>&1; then
 fi
 
 next_version=""
-if [[ "$VERSION" =~ "alpha" ]] || [[ "$VERSION" =~ "beta" ]]|| [[ "$VERSION" =~ "M" ]]; then
-  next_version=$(echo "$VERSION" | sed 's/-.*//')
+if [[ "$VERSION" =~ -(alpha|beta|M|rc)[0-9]*$ ]]; then
+  next_version="${VERSION%%-*}"
 fi
 
 mvn versions:set -DnewVersion=$VERSION versions:commit
