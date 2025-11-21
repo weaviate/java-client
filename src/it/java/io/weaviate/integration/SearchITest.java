3aef0cf81bad681a0fe6eedec4d38ab3423777c0
dyma solovei <dyma@weaviate.io>
2025-11-21 15:42:43 +0100

    chore: fix malformed import

--- src/it/java/io/weaviate/integration/SearchITest.java
+++ src/it/java/io/weaviate/integration/SearchITest.java
@@ -21,6 +21,7 @@ import org.junit.rules.TestRule;
 import io.weaviate.ConcurrentTest;
 import io.weaviate.client6.v1.api.WeaviateApiException;
 import io.weaviate.client6.v1.api.WeaviateClient;
+import io.weaviate.client6.v1.api.collections.Generative;
 import io.weaviate.client6.v1.api.collections.Property;
 import io.weaviate.client6.v1.api.collections.ReferenceProperty;
 import io.weaviate.client6.v1.api.collections.Reranker;
@@ -29,7 +30,7 @@ import io.weaviate.client6.v1.api.collections.Vectors;
 import io.weaviate.client6.v1.api.collections.WeaviateMetadata;
 import io.weaviate.client6.v1.api.collections.data.Reference;
 import io.weaviate.client6.v1.api.collections.data.WriteWeaviateObject;
-import io.weaviate.client6.v1.api.collections.generate.
+import io.weaviate.client6.v1.api.collections.generate.GenerativeObject;
 import io.weaviate.client6.v1.api.collections.generate.TaskOutput;
 import io.weaviate.client6.v1.api.collections.generative.DummyGenerative;
 import io.weaviate.client6.v1.api.collections.query.Filter;
