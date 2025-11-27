5a4b54273120c4580c828d08c39d18147600f877
JP Hwang <61258750+databyjp@users.noreply.github.com>
2025-11-27 13:56:32 +0000

    Apply suggestions from code review
    Co-authored-by: dyma solovei <53943884+bevzzz@users.noreply.github.com>

--- src/main/java/io/weaviate/client6/v1/api/backup/CompressionLevel.java
+++ src/main/java/io/weaviate/client6/v1/api/backup/CompressionLevel.java
@@ -11,7 +11,7 @@ public enum CompressionLevel {
   BEST_SPEED,
   /** Use compression algorithm that prioritizes compression quality. */
   @SerializedName("BestCompression")
-  BEST_COMPRESSION;
+  BEST_COMPRESSION,
   /** Use ZSTD compression algorithm at default (balanced) settings. */
   @SerializedName("ZstdDefaultCompression")
   ZSTD_DEFAULT,
@@ -20,7 +20,7 @@ public enum CompressionLevel {
   ZSTD_BEST_SPEED,
   /** Use ZSTD compression algorithm and prioritize compression quality. */
   @SerializedName("ZstdBestCompression")
-  ZSTD_BEST_COMPRESSION;
+  ZSTD_BEST_COMPRESSION,
   /** Do not use compression. */
   @SerializedName("NoCompression")
   NO_COMPRESSION;
